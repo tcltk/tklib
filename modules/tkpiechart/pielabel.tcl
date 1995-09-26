77172d0f7182bbf2d41bb75cd9cf75204113aea4
jfontain <jfontain>
1995-09-26 17:07:35 +0000

    in constructor, added -offset option, calculate font height.

--- modules/tkpiechart/pielabel.tcl
+++ modules/tkpiechart/pielabel.tcl
@@ -1,14 +1,22 @@
-set rcsId {$Id: pielabel.tcl,v 1.1 1995/09/26 12:54:05 jfontain Exp $}
+set rcsId {$Id: pielabel.tcl,v 1.2 1995/09/26 17:07:35 jfontain Exp $}
 
-proc pieLabeller::pieLabeller {id pie args} {
+proc pieLabeller::pieLabeller {id pieId args} {
+    global pie
     allowVirtualProceduresIn pieLabeller
 
     # set options default then parse switched options
-    array set option {-font -adobe-helvetica-medium-r-*-120-*}
+    array set option {-font -adobe-helvetica-medium-r-*-120-* -offset 5}
     array set option $args
 
     set pieLabeller($id,font) $option(-font)
-    set pieLabeller($id,pie) $pie
+    set pieLabeller($id,offset) $option(-offset)
+
+    set canvas $pie($pieId,canvas)
+    set temporary [$canvas create text 0 0 -font $pieLabeller($id,font)]
+    set box [$canvas bbox $temporary]
+    $canvas delete $temporary
+    set pieLabeller($id,fontHeight) [expr [lindex $box 3]-[lindex $box 1]]
+    set pieLabeller($id,pie) $pieId
 }
 
 proc pieLabeller::~pieLabeller {id} {
