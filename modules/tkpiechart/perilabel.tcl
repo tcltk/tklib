573c8704685c8a3f237aebc3a52a639b28a3c041
jfontain <jfontain>
1995-10-04 23:00:10 +0000

    renamed class to piePeripheralLabeller.

--- modules/tkpiechart/perilabel.tcl
+++ modules/tkpiechart/perilabel.tcl
@@ -1,11 +1,11 @@
-set rcsId {$Id: perilabel.tcl,v 1.1 1995/10/04 22:40:48 jfontain Exp $}
+set rcsId {$Id: perilabel.tcl,v 1.2 1995/10/04 23:00:10 jfontain Exp $}
 
 source pielabel.tcl
 
-proc peripheralBoxLabeller::peripheralBoxLabeller {id canvas args} {
+proc piePeripheralLabeller::piePeripheralLabeller {id canvas args} {
     eval pieLabeller::pieLabeller $id $canvas $args
 }
 
-proc peripheralBoxLabeller::~peripheralBoxLabeller {id} {}
+proc piePeripheralLabeller::~piePeripheralLabeller {id} {}
 
-proc peripheralBoxLabeller::position {id label} {}
+proc piePeripheralLabeller::position {id label} {}
