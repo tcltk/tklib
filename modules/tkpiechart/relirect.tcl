074e738101f23cfc83dcdc89ff0139933e0dfdda
jfontain <jfontain>
2001-02-11 22:06:40 +0000

    added -backgrouund option. added set-background{} and update{}.

--- modules/tkpiechart/relirect.tcl
+++ modules/tkpiechart/relirect.tcl
@@ -1,4 +1,4 @@
-set rcsId {$Id: relirect.tcl,v 1.1 2000/12/24 16:27:56 jfontain Exp $}
+set rcsId {$Id: relirect.tcl,v 1.2 2001/02/11 22:06:40 jfontain Exp $}
 
 
 class canvasReliefRectangle {
@@ -14,11 +14,42 @@ class canvasReliefRectangle {
         $($this,canvas) delete canvasReliefRectangle($this)
     }
 
-    proc options {this} {                                                      ;# force relief initialization for color calculations
+    proc options {this} {                                                  ;# force background initialization for color calculations
         return [list\
+            [list -background white]\
             [list -coordinates {0 0 0 0} {0 0 0 0}]\
-            [list -relief flat]\
+            [list -relief flat flat]\
         ]
+    }
+
+    proc set-background {this value} {                                                           ;# algorithm stolen from tkUnix3d.c
+        set intensity 65535                                                                                     ;# maximum intensity
+        foreach {red green blue} [winfo rgb $($this,canvas) $value] {}
+        if {(($red*0.5*$red)+($green*1.0*$green)+($blue*0.28*$blue))<($intensity*0.05*$intensity)} {
+            set ($this,dark) [format {#%04X%04X%04X}\
+                [expr {($intensity+(3*$red))/4}] [expr {($intensity+(3*$green))/4}] [expr {($intensity+(3*$blue))/4}]\
+            ]
+        } else {
+            set ($this,dark) [format {#%04X%04X%04X} [expr {(60*$red)/100}] [expr {(60*$green)/100}] [expr {(60*$blue)/100}]]
+        }
+        if {$green>($intensity*0.95)} {
+            set ($this,light) [format {#%04X%04X%04X} [expr {(90*$red)/100}] [expr {(90*$green)/100}] [expr {(90*$blue)/100}]]
+        } else {
+            set tmp1 [expr {(14*$red)/10}]
+            if {$tmp1>$intensity} {set tmp1 $intensity}
+            set tmp2 [expr {($intensity+$red)/2}]
+            set lightRed [expr {($tmp1>$tmp2)?$tmp1:$tmp2}]
+            set tmp1 [expr {(14*$green)/10}]
+            if {$tmp1>$intensity} {set tmp1 $intensity}
+            set tmp2 [expr {($intensity+$green)/2}]
+            set lightGreen [expr {($tmp1>$tmp2)?$tmp1:$tmp2}]
+            set tmp1 [expr {(14*$blue)/10}]
+            if {$tmp1>$intensity} {set tmp1 $intensity}
+            set tmp2 [expr {($intensity+$blue)/2}]
+            set lightBlue [expr {($tmp1>$tmp2)?$tmp1:$tmp2}]
+            set ($this,light) [format {#%04X%04X%04X} $lightRed $lightGreen $lightBlue]
+        }
+        update $this
     }
 
     proc set-coordinates {this value} {
@@ -28,17 +59,22 @@ class canvasReliefRectangle {
     }
 
     proc set-relief {this value} {
-        switch $value {
+        if {![info exists ($this,dark)]} return                                                         ;# colors not yet calculated
+        update $this
+    }
+
+    proc update {this} {
+        switch $switched::($this,-relief) {
             flat {
-                $($this,canvas) itemconfigure canvasReliefRectangle($this) -fill #D9D9D9
+                $($this,canvas) itemconfigure canvasReliefRectangle($this) -fill $switched::($this,-background)
             }
             raised {
-                $($this,canvas) itemconfigure $($this,topLeft) -fill white
-                $($this,canvas) itemconfigure $($this,bottomRight) -fill #828282
+                $($this,canvas) itemconfigure $($this,topLeft) -fill $($this,light)
+                $($this,canvas) itemconfigure $($this,bottomRight) -fill $($this,dark)
             }
             sunken {
-                $($this,canvas) itemconfigure $($this,topLeft) -fill #828282
-                $($this,canvas) itemconfigure $($this,bottomRight) -fill white
+                $($this,canvas) itemconfigure $($this,topLeft) -fill $($this,dark)
+                $($this,canvas) itemconfigure $($this,bottomRight) -fill $($this,light)
             }
             default {
                 error "bad relief value \"$value\": must be flat, raised or sunken"
