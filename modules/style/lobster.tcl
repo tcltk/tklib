dd5e71d337270cb2ae39ffeb1163161af12c64e1
davidw <davidw>
2004-03-17 21:27:46 +0000

    * style.tcl: Created style package.

---
+++ modules/style/lobster.tcl
@@ -0,0 +1,54 @@
+# lobster.tcl --
+
+# The code formerly known as "gtklook" on the Tcl'ers
+# wiki.  Most of this code was originally written by Jeremy Collins.
+
+# $Id: lobster.tcl,v 1.1 2004/03/17 21:27:47 davidw Exp $
+
+package provide style::lobster 0.1
+
+namespace eval styles::lobster {
+    if { [tk windowingsystem] == "x11" } {
+	option add *borderWidth 1 widgetDefault
+	option add *activeBorderWidth 1 widgetDefault
+	option add *selectBorderWidth 1 widgetDefault
+	option add *font -adobe-helvetica-medium-r-normal-*-12-*-*-*-*-*-* widgetDefault
+
+	option add *padX 2 widgetDefault
+	option add *padY 4 widgetDefault
+
+	option add *Listbox.background white widgetDefault
+	option add *Listbox.selectBorderWidth 0 widgetDefault
+	option add *Listbox.selectForeground white widgetDefault
+	option add *Listbox.selectBackground #4a6984 widgetDefault
+
+	option add *Entry.background white widgetDefault
+	option add *Entry.foreground black widgetDefault
+	option add *Entry.selectBorderWidth 0 widgetDefault
+	option add *Entry.selectForeground white widgetDefault
+	option add *Entry.selectBackground #4a6984 widgetDefault
+
+	option add *Text.background white widgetDefault
+	option add *Text.selectBorderWidth 0 widgetDefault
+	option add *Text.selectForeground white widgetDefault
+	option add *Text.selectBackground #4a6984 widgetDefault
+
+	option add *Menu.activeBackground #4a6984 widgetDefault
+	option add *Menu.activeForeground white widgetDefault
+	option add *Menu.activeBorderWidth 0 widgetDefault
+	option add *Menu.highlightThickness 0 widgetDefault
+	option add *Menu.borderWidth 2 widgetDefault
+
+	option add *Menubutton.activeBackground #4a6984 widgetDefault
+	option add *Menubutton.activeForeground white widgetDefault
+	option add *Menubutton.activeBorderWidth 0 widgetDefault
+	option add *Menubutton.highlightThickness 0 widgetDefault
+	option add *Menubutton.borderWidth 0 widgetDefault
+
+	option add *Labelframe.borderWidth 2
+	option add *Frame.borderWidth 2
+
+	option add *highlightThickness 0 widgetDefault
+	option add *troughColor #bdb6ad widgetDefault
+    }
+}
