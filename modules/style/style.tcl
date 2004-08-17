9fb81b1586ebbc5a9f3ed352773f13b2ab2016b8
hobbs <hobbs>
2004-08-17 19:09:23 +0000

    * as.tcl:       Change style::use to call style::${style}::init 	* style.tcl:    procedure, so a simple package require doesn't 	* lobster.tcl:  actually do anything.  Add init to lobster and 	* pkgIndex.tcl: correct namespace usage.  Move lobster and style 	to v0.2.  style::use now takes args to pass to init, eg: 	style::use as -priority 80

--- modules/style/style.tcl
+++ modules/style/style.tcl
@@ -1,10 +1,11 @@
 # style.tcl -- Styles for Tk.
 
-# $Id: style.tcl,v 1.2 2004/03/18 08:56:47 davidw Exp $
+# $Id: style.tcl,v 1.3 2004/08/17 19:09:23 hobbs Exp $
 
 # Copyright 2004 David N. Welton <davidw@dedasys.com>
+# Copyright 2004 ActiveState Corporation
 
-package provide style 0.1
+package provide style 0.2
 
 namespace eval style {
     # Available styles
@@ -22,10 +23,11 @@ proc style::names {} {
 
 # style::use --
 #
-#	Untill I see a better way of doing it, this is just a wrapper
+#	Until I see a better way of doing it, this is just a wrapper
 #	for package require.  The problem is that 'use'ing different
 #	styles won't undo the changes made by previous styles.
 
-proc style::use {newstyle} {
+proc style::use {newstyle args} {
     package require style::${newstyle}
+    eval [linsert $args 0 style::${newstyle}::init]
 }
