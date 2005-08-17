2d8edcfa0c3461a9c576ff336b1a51c7cc35a719
Andreas Kupries <andreas.kupries@gmail.com>
2005-08-17 22:50:17 +0000

    * swaplist.man: 	* swaplist.tcl: Added the missing dependency on Tk.

--- modules/swaplist/swaplist.tcl
+++ modules/swaplist/swaplist.tcl
@@ -7,8 +7,9 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: swaplist.tcl,v 1.3 2005/04/02 07:01:24 andreas_kupries Exp $
+# RCS: @(#) $Id: swaplist.tcl,v 1.4 2005/08/17 22:50:17 andreas_kupries Exp $
 
+package require Tk
 package provide swaplist 0.1
 
 namespace eval swaplist {
