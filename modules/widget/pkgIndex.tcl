d07486c9dcad45245e617ce6077cb712e8a0b85a
Jeffrey Hobbs <jeffh@activestate.com>
2005-08-25 19:48:52 +0000

    * dialog.tcl: don't require 'name' in dialog button add, allow 	widget pathnames to be inserted, up to v1.1

--- modules/widget/pkgIndex.tcl
+++ modules/widget/pkgIndex.tcl
@@ -13,7 +13,7 @@ package ifneeded widget::all 1.0 {
 package ifneeded widget 3.0 [list source [file join $dir widget.tcl]]
 package ifneeded widget::scrolledwindow 1.0 \
     [list source [file join $dir scrollw.tcl]]
-package ifneeded widget::dialog 1.0 \
+package ifneeded widget::dialog 1.1 \
     [list source [file join $dir dialog.tcl]]
 package ifneeded widget::superframe 1.0 \
     [list source [file join $dir superframe.tcl]]
