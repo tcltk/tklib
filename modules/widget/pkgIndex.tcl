363a84e3dc2cea6e80bf3f1ecd27f4e9ce3b08c1
Jeffrey Hobbs <jeffh@activestate.com>
2005-09-21 17:43:31 +0000

    * widget.man, pkgIndex.tcl, ruler.tcl: add widget::ruler widget 	and widget::screenruler dialog

--- modules/widget/pkgIndex.tcl
+++ modules/widget/pkgIndex.tcl
@@ -7,15 +7,26 @@ package ifneeded widget::all 1.0 {
     package require widget::scrolledwindow
     package require widget::dialog
     package require widget::superframe
+    package require widget::panelframe
+    package require widget::ruler ; # includes screenruler
     package provide widget::all 1.0
 }
 
 package ifneeded widget 3.0 [list source [file join $dir widget.tcl]]
+
 package ifneeded widget::scrolledwindow 1.0 \
     [list source [file join $dir scrollw.tcl]]
+
 package ifneeded widget::dialog 1.1 \
     [list source [file join $dir dialog.tcl]]
+
 package ifneeded widget::superframe 1.0 \
     [list source [file join $dir superframe.tcl]]
+
 package ifneeded widget::panelframe 1.0 \
     [list source [file join $dir panelframe.tcl]]
+
+package ifneeded widget::screenruler 1.0 \
+    [list source [file join $dir ruler.tcl]]
+package ifneeded widget::ruler 1.0 \
+    [list source [file join $dir ruler.tcl]]
