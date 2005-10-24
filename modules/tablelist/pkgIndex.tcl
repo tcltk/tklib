da60c2450d6a7286cd62bb3f070ded070c1dc8d6
Andreas Kupries <akupries@shaw.ca>
2005-10-24 21:54:31 +0000

    * pkgIndex.tcl: Reorganized things a bit. Procedures to remove 	  quoting issues and make code more readable. Unrolled the loops 	  to make code accessible to extraction of name/version 	  information by sak tool.
    2005-10-2x Csaba Nemethi
    
    	* Unknown changes -- This entry is a placeholder
    	  Please, always update the ChangeLog before a commit !!

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -4,25 +4,37 @@
 # Copyright (c) 2000-2005  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-if {[string compare $::tcl_platform(platform) "macintosh"] == 0} {
-    #
-    # We need to do this here instead of in tablelist.tcl, because of
-    # a bug in [info script] in some Tcl releases for the Macintosh.
-    #
-    namespace eval tablelist {}
-    set tablelist::library $dir
+namespace eval ::tablelist {}
+
+proc ::tablelist::Load {dir} {
+    if {[string compare $::tcl_platform(platform) "macintosh"] == 0} {
+	#
+	# We need to do this here instead of in tablelist.tcl, because of
+	# a bug in [info script] in some Tcl releases for the Macintosh.
+	#
+	set ::tablelist::library $dir
+    }
+
+    source [file join $dir tablelistPublic.tcl]
+    source [file join $dir tablelist.tcl]
+    rename ::tablelist::Load {}
 }
 
-foreach package {Tablelist tablelist} {
-    package ifneeded $package 4.2 "
-	[list source [file join $dir tablelistPublic.tcl]]
-	[list source [file join $dir tablelist.tcl]]
-    "
+proc ::tablelist::LoadTile {dir} {
+    if {[string compare $::tcl_platform(platform) "macintosh"] == 0} {
+	#
+	# We need to do this here instead of in tablelist.tcl, because of
+	# a bug in [info script] in some Tcl releases for the Macintosh.
+	#
+	set ::tablelist::library $dir
+    }
+
+    source [file join $dir tablelistPublic.tcl]
+    source [file join $dir tablelist_tile.tcl]
+    rename ::tablelist::LoadTile {}
 }
 
-foreach package {Tablelist_tile tablelist_tile} {
-    package ifneeded $package 4.2 "
-	[list source [file join $dir tablelistPublic.tcl]]
-	[list source [file join $dir tablelist_tile.tcl]]
-    "
-}
+package ifneeded Tablelist      4.2 [list ::tablelist::Load     $dir]
+package ifneeded tablelist      4.2 [list ::tablelist::Load     $dir]
+package ifneeded Tablelist_tile 4.2 [list ::tablelist::LoadTile $dir]
+package ifneeded tablelist_tile 4.2 [list ::tablelist::LoadTile $dir]
