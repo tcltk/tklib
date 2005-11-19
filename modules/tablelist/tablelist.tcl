35b925a0b9970da17eaf2da99a0b99bfe529ba40
nemethi <nemethi>
2005-11-19 18:11:29 +0000

    * *.tcl:         Bumped the version number to 4.2.1. 	* COPYRIGHT.txt:

--- modules/tablelist/tablelist.tcl
+++ modules/tablelist/tablelist.tcl
@@ -7,8 +7,8 @@
 package require Tcl 8
 package require Tk  8
 
-package provide Tablelist 4.2
-package provide tablelist 4.2
+package provide Tablelist 4.2.1
+package provide tablelist 4.2.1
 
 set tablelist::usingTile 0
 trace variable tablelist::usingTile wu "tablelist::restoreUsingTile 0"
