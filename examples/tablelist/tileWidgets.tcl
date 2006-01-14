a1ce9d02f4347bca536ab1aff5ec7a5056b09811
nemethi <nemethi>
2006-01-14 22:01:18 +0000

    2006-01-14  Csaba Nemethi <csaba.nemethi@t-online.de>
    	* ../../examples/tablelist/*.tcl: Minor updates and improvements.

--- examples/tablelist/tileWidgets.tcl
+++ examples/tablelist/tileWidgets.tcl
@@ -6,7 +6,7 @@ exec wish "$0" ${1+"$@"}
 # Demonstrates the interactive tablelist cell editing with the aid of some
 # widgets from the tile package and of the Tk core spinbox widget.
 #
-# Copyright (c) 2005  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2005-2006  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
 package require Tablelist_tile
