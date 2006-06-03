bc17544eae163e337f74f5efa70cc12fe744e1c3
Csaba Nemethi <csaba.nemethi@t-online.de>
2006-06-03 18:30:23 +0000

    * doc/tablelist.html:                       Updated a few option 	* doc/tablelistThemeDefs.html:              database settings. 	* ../../examples/tablelist/option_tile.tcl:

--- examples/tablelist/option_tile.tcl
+++ examples/tablelist/option_tile.tcl
@@ -13,11 +13,16 @@ if {[tk windowingsystem] eq "x11"} {
     option add *Font			"Helvetica -12"
 }
 tablelist::setThemeDefaults
-if {$tile::currentTheme ne "aqua"} {
+if {$tile::currentTheme eq "aqua"} {
+    option add *Listbox.selectBackground \
+	       $tablelist::themeDefaults(-selectbackground)
+    option add *Listbox.selectForeground \
+	       $tablelist::themeDefaults(-selectforeground)
+} else {
     option add *selectBackground  $tablelist::themeDefaults(-selectbackground)
     option add *selectForeground  $tablelist::themeDefaults(-selectforeground)
-    option add *selectBorderWidth $tablelist::themeDefaults(-selectborderwidth)
 }
+option add *selectBorderWidth     $tablelist::themeDefaults(-selectborderwidth)
 option add *Tablelist.activeStyle	frame
 option add *Tablelist.background	gray98
 option add *Tablelist.stripeBackground	#e0e8f0
