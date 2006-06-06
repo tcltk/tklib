6aa1e8746d15a78fb0942901ffb16aa02cc6d2d4
Andreas Kupries <andreas.kupries@gmail.com>
2006-06-06 23:43:31 +0000

    * tablelistPublic.tcl: Sipmlified the handling of $dir, using 	* tablelist.tcl:       essentially a personalized 'info script' 	* tablelist_tile.tcl:  defined in the 'ifneeded' script. Also 	* pkgIndex.tcl:        loading public from the package files, not 	                       not the index anymore.

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -4,37 +4,7 @@
 # Copyright (c) 2000-2006  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-namespace eval ::tablelist {}
-
-proc ::tablelist::Load {dir} {
-    if {[string compare $::tcl_platform(platform) "macintosh"] == 0} {
-	#
-	# We need to do this here instead of in tablelistPublic.tcl, because
-	# of a bug in [info script] in some Tcl releases for the Macintosh.
-	#
-	variable library $dir
-    }
-
-    uplevel #0 [list source [file join $dir tablelistPublic.tcl]]
-    uplevel #0 [list source [file join $dir tablelist.tcl]]
-    rename ::tablelist::Load {}
-}
-
-proc ::tablelist::LoadTile {dir} {
-    if {[string compare $::tcl_platform(platform) "macintosh"] == 0} {
-	#
-	# We need to do this here instead of in tablelistPublic.tcl, because
-	# of a bug in [info script] in some Tcl releases for the Macintosh.
-	#
-	variable library $dir
-    }
-
-    uplevel #0 [list source [file join $dir tablelistPublic.tcl]]
-    uplevel #0 [list source [file join $dir tablelist_tile.tcl]]
-    rename ::tablelist::LoadTile {}
-}
-
-package ifneeded Tablelist      4.4 [list ::tablelist::Load     $dir]
-package ifneeded tablelist      4.4 [list ::tablelist::Load     $dir]
-package ifneeded Tablelist_tile 4.4 [list ::tablelist::LoadTile $dir]
-package ifneeded tablelist_tile 4.4 [list ::tablelist::LoadTile $dir]
+package ifneeded Tablelist      4.4 "namespace eval ::tablelist {proc DIR {} {return [list $dir]}} ; [list source [file join $dir tablelist.tcl]]"
+package ifneeded tablelist      4.4 "namespace eval ::tablelist {proc DIR {} {return [list $dir]}} ; [list source [file join $dir tablelist.tcl]]"
+package ifneeded Tablelist_tile 4.4 "namespace eval ::tablelist {proc DIR {} {return [list $dir]}} ; [list source [file join $dir tablelist_tile.tcl]]"
+package ifneeded tablelist_tile 4.4 "namespace eval ::tablelist {proc DIR {} {return [list $dir]}} ; [list source [file join $dir tablelist_tile.tcl]]"
