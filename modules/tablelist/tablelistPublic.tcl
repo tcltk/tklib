d21da5138c56b730df6aba672fbc06a79e0eae47
nemethi <nemethi>
2006-06-15 10:34:09 +0000

    * *.tcl:         Bumped the version number to 4.4.1. 	* COPYRIGHT.txt:

--- modules/tablelist/tablelistPublic.tcl
+++ modules/tablelist/tablelistPublic.tcl
@@ -5,24 +5,14 @@
 # Copyright (c) 2000-2006  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package provide tablelist::common 4.4
+package provide tablelist::common 4.4.1
 
 namespace eval ::tablelist {
     #
     # Public variables:
     #
-
-    variable version	4.4
-    variable library	[::tablelist::DIR]
-    variable usingTile	; # set in tablelist.tcl or tablelist_tile.tcl
-
-    proc useTile {bool} {
-	variable usingTile $bool
-	trace variable ::tablelist::usingTile wu \
-		[list ::tablelist::restoreUsingTile $bool]
-	rename ::tablelist::useTile {}
-	return
-    }
+    variable version	4.4.1
+    variable library	[DIR]
 
     #
     # Creates a new tablelist widget:
@@ -60,11 +50,40 @@ namespace eval ::tablelist {
     namespace export	setThemeDefaults
 }
 
-# Everything else needed is lazily loaded on demand, via the
-# dispatcher set up in the subdirectory "scripts", see the file
-# "tclIndex".
+#
+# The following procedure, invoked in "tablelist.tcl" and "tablelist_tile.tcl",
+# sets the variable ::tablelist::usingTile to the given value and sets a trace
+# on this variable.
+#
+proc ::tablelist::useTile {bool} {
+    variable usingTile $bool
+    trace variable usingTile wu [list ::tablelist::restoreUsingTile $bool]
+}
 
+#
+# The following trace procedure is executed whenever the variable
+# ::tablelist::usingTile is written or unset.  It restores the variable to its
+# original value, given by the first argument.
+#
+proc ::tablelist::restoreUsingTile {origVal varName index op} {
+    variable usingTile $origVal
+    switch $op {
+	w {
+	    return -code error "it is not allowed to use both Tablelist and\
+				Tablelist_tile in the same application"
+	}
+	u {
+	    trace variable usingTile wu \
+		  [list ::tablelist::restoreUsingTile $origVal]
+	}
+    }
+}
+
+interp alias {} ::tk::frame {} ::frame
+interp alias {} ::tk::label {} ::label
+
+#
+# Everything else needed is lazily loaded on demand, via the dispatcher
+# set up in the subdirectory "scripts" (see the file "tclIndex").
+#
 lappend auto_path [file join $::tablelist::library scripts]
-
-interp alias {} tk::frame {} ::frame
-interp alias {} tk::label {} ::label
