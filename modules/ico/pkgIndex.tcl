e18fa98b11bc5d37db108d5b23d0d729d584cfeb
Jeffrey Hobbs <jeffh@activestate.com>
2006-08-04 16:37:13 +0000

    * ico0.tcl: added back ico 0.3 for compatibility 	* pkgIndex.tcl: note both 0.3 and 1.0 exist

--- modules/ico/pkgIndex.tcl
+++ modules/ico/pkgIndex.tcl
@@ -3,6 +3,7 @@
 # Copyright (c) 2003 ActiveState Corporation.
 # All rights reserved.
 #
-# RCS: @(#) $Id: pkgIndex.tcl,v 1.3 2006/07/12 03:52:47 afaupell Exp $
+# RCS: @(#) $Id: pkgIndex.tcl,v 1.4 2006/08/04 16:37:13 hobbs Exp $
 
+package ifneeded ico 0.3 [list source [file join $dir ico0.tcl]]
 package ifneeded ico 1.0 [list source [file join $dir ico.tcl]]
