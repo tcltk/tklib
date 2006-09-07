dd8f430a9f8f7e42805bf220e37c5b2378a8ceb7
Andreas Kupries <akupries@shaw.ca>
2006-09-07 21:22:18 +0000

    * pkgIndex.tcl (::tcl::pkgindex): Reworked the 'pkindex' command 	  to make it more general, and more susceptible to programmatic 	  analysis (meta data extraction).

--- modules/widget/pkgIndex.tcl
+++ modules/widget/pkgIndex.tcl
@@ -1,27 +1,28 @@
-# Tcl package index file, version 1.0
-
-if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-
-namespace eval ::widget {}
-proc ::widget::pkgindex {dir} {
-    set allpkgs [list]
-    # Keep alphabetical for sanity
-    foreach {pkg ver file} {
-	widget			3.0	widget.tcl
-	widget::dialog		1.2	dialog.tcl
-	widget::menuentry	1.0	mentry.tcl
-	widget::panelframe	1.0	panelframe.tcl
-	widget::ruler		1.0	ruler.tcl
-	widget::screenruler	1.1	ruler.tcl
-	widget::scrolledwindow	1.1	scrollw.tcl
-	widget::statusbar	1.1	statusbar.tcl
-	widget::superframe	1.0	superframe.tcl
-	widget::toolbar		1.0	toolbar.tcl
-    } {
-	lappend allpkgs [list package require $pkg $ver]
-	package ifneeded $pkg $ver [list source [file join $dir $file]]
+# Tcl Package Index File 1.0
+if {![llength [info commands ::tcl::pkgindex]]} {
+    proc ::tcl::pkgindex {dir bundle bundlev packages} {
+	set allpkgs [list]
+	foreach {pkg ver file} $packages {
+	    lappend allpkgs [list package require $pkg $ver]
+	    package ifneeded $pkg $ver [list source [file join $dir $file]]
+	}
+	if {$bundle != ""} {
+	    lappend allpkgs [list package provide $bundle $bundlev]
+	    package ifneeded $bundle $bundlev [join $allpkgs \n]
+	}
+	return
     }
-    lappend allpkgs {package provide widget::all 1.1}
-    package ifneeded widget::all 1.1 [join $allpkgs \n]
 }
-::widget::pkgindex $dir
+if {![package vsatisfies [package provide Tcl] 8.4]} {return}
+::tcl::pkgindex $dir widget::all 1.1 {
+    widget			3.0	widget.tcl
+    widget::dialog		1.2	dialog.tcl
+    widget::menuentry		1.0	mentry.tcl
+    widget::panelframe		1.0	panelframe.tcl
+    widget::ruler		1.0	ruler.tcl
+    widget::screenruler		1.1	ruler.tcl
+    widget::scrolledwindow	1.1	scrollw.tcl
+    widget::statusbar		1.1	statusbar.tcl
+    widget::superframe		1.0	superframe.tcl
+    widget::toolbar		1.0	toolbar.tcl
+}
