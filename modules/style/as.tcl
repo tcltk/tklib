71a42afbb18585914d7979b880d45c61745d6ce3
Jeffrey Hobbs <jeffh@activestate.com>
2006-09-28 22:21:46 +0000

    * as.tcl (style::as::MouseWheel): handle Shift-MouseWheel 	correctly as the possible incoming event

--- modules/style/as.tcl
+++ modules/style/as.tcl
@@ -411,19 +411,21 @@ proc style::as::init_panedwindow {args} {
 ## MouseWheel
 ##
 proc style::as::MouseWheel {wFired X Y D {shifted 0}} {
+    # Set event to check based on call
+    set evt "<[expr {$shifted?{Shift-}:{}}]MouseWheel>"
     # do not double-fire in case the class already has a binding
-    if {[bind [winfo class $wFired] <MouseWheel>] ne ""} { return }
+    if {[bind [winfo class $wFired] $evt] ne ""} { return }
     # obtain the window the mouse is over
     set w [winfo containing $X $Y]
     # if we are outside the app, try and scroll the focus widget
     if {![winfo exists $w]} { catch {set w [focus]} }
     if {[winfo exists $w]} {
-	if {[bind $w <MouseWheel>] ne ""} {
+	if {[bind $w $evt] ne ""} {
 	    # Awkward ... this widget has a MouseWheel binding, but to
 	    # trigger successfully in it, we must give it focus.
 	    catch {focus} old
 	    if {$w ne $old} { focus $w }
-	    event generate $w <MouseWheel> -rootx $X -rooty $Y -delta $D
+	    event generate $w $evt -rootx $X -rooty $Y -delta $D
 	    if {$w ne $old} { focus $old }
 	    return
 	}
@@ -440,6 +442,8 @@ proc style::as::MouseWheel {wFired X Y D {shifted 0}} {
 	} else {
 	    set cmd [list $w [expr {$shifted ? "xview" : "yview"}] \
 			 scroll $delta units]
+	    # Walking up to find the proper widget handles cases like
+	    # embedded widgets in a canvas
 	    while {[catch $cmd] && [winfo toplevel $w] ne $w} {
 		set w [winfo parent $w]
 	    }
