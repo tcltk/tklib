318451d2e6ac5b0d3fb7a9902086a8b067eff37e
Jeffrey Hobbs <jeffh@activestate.com>
2006-12-14 18:16:43 +0000

    reflect updated ico version

--- modules/ico/pkgIndex.tcl
+++ modules/ico/pkgIndex.tcl
@@ -3,7 +3,7 @@
 # Copyright (c) 2003 ActiveState Corporation.
 # All rights reserved.
 #
-# RCS: @(#) $Id: pkgIndex.tcl,v 1.4 2006/08/04 16:37:13 hobbs Exp $
+# RCS: @(#) $Id: pkgIndex.tcl,v 1.5 2006/12/14 18:16:43 hobbs Exp $
 
 package ifneeded ico 0.3 [list source [file join $dir ico0.tcl]]
-package ifneeded ico 1.0 [list source [file join $dir ico.tcl]]
+package ifneeded ico 1.0.1 [list source [file join $dir ico.tcl]]
