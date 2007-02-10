f0dcdf1a142c9c6868c547947e057e20b3932bf3
Arjen Markus <arjen.markus@deltares.nl>
2007-02-10 09:04:41 +0000

    Documentation on legend commands added. Raised version to 1.2

--- modules/plotchart/pkgIndex.tcl
+++ modules/plotchart/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.3]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded Plotchart 1.1 [list source [file join $dir plotchart.tcl]]
+package ifneeded Plotchart 1.2 [list source [file join $dir plotchart.tcl]]
