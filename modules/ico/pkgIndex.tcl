a137463b09e9d399be7fcd18c0eb3bcb2a9723dd
Jeffrey Hobbs <jeffh@activestate.com>
2007-02-23 22:59:59 +0000

    * ico.tcl (::ico::getFileIcon): fixed for better overall support. 	[Bug 1660234] (Griffiths). 	Bumped version to 1.0.2

--- modules/ico/pkgIndex.tcl
+++ modules/ico/pkgIndex.tcl
@@ -3,7 +3,7 @@
 # Copyright (c) 2003 ActiveState Corporation.
 # All rights reserved.
 #
-# RCS: @(#) $Id: pkgIndex.tcl,v 1.5 2006/12/14 18:16:43 hobbs Exp $
+# RCS: @(#) $Id: pkgIndex.tcl,v 1.6 2007/02/23 22:59:59 hobbs Exp $
 
 package ifneeded ico 0.3 [list source [file join $dir ico0.tcl]]
-package ifneeded ico 1.0.1 [list source [file join $dir ico.tcl]]
+package ifneeded ico 1.0.2 [list source [file join $dir ico.tcl]]
