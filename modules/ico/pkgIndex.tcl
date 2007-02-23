703e5299bbff8180df75af8567a44fc9ba406358
Jeffrey Hobbs <jeffh@activestate.com>
2007-02-23 23:28:33 +0000

    * ico0.tcl (::ico::readDIBFromData): correct row calc for edge 	case.  Bump version to 0.3.1

--- modules/ico/pkgIndex.tcl
+++ modules/ico/pkgIndex.tcl
@@ -3,7 +3,7 @@
 # Copyright (c) 2003 ActiveState Corporation.
 # All rights reserved.
 #
-# RCS: @(#) $Id: pkgIndex.tcl,v 1.6 2007/02/23 22:59:59 hobbs Exp $
+# RCS: @(#) $Id: pkgIndex.tcl,v 1.7 2007/02/23 23:28:33 hobbs Exp $
 
-package ifneeded ico 0.3 [list source [file join $dir ico0.tcl]]
+package ifneeded ico 0.3.1 [list source [file join $dir ico0.tcl]]
 package ifneeded ico 1.0.2 [list source [file join $dir ico.tcl]]
