59c3e0b03fb228d861612e9385ed8c2c4ca66aac
nemethi <nemethi>
2007-03-23 20:06:37 +0000

    * *.tcl:         Bumped the version number to 4.6.1. 	* COPYRIGHT.txt:

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -7,20 +7,20 @@
 #
 # Regular packages:
 #
-package ifneeded tablelist         4.6 \
+package ifneeded tablelist         4.6.1 \
 	[list source [file join $dir tablelist.tcl]]
-package ifneeded tablelist_tile    4.6 \
+package ifneeded tablelist_tile    4.6.1 \
 	[list source [file join $dir tablelist_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Tablelist         4.6 { package require tablelist }
-package ifneeded Tablelist_tile    4.6 { package require tablelist_tile }
+package ifneeded Tablelist         4.6.1 { package require tablelist }
+package ifneeded Tablelist_tile    4.6.1 { package require tablelist_tile }
 
 #
 # Code common to all packages:
 #
-package ifneeded tablelist::common 4.6 \
+package ifneeded tablelist::common 4.6.1 \
         "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
 	 source [list [file join $dir tablelistPublic.tcl]]"
