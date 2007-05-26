8f478e1367a13031688c5bb7f596f2004dec958d
Arjen Markus <arjen.markus@deltares.nl>
2007-05-26 07:56:27 +0000

    Added right axis

--- examples/plotchart/plotdemos1.tcl
+++ examples/plotchart/plotdemos1.tcl
@@ -32,6 +32,7 @@ canvas .v.c3 -background white -width 400 -height 200
 pack   .v.c .v.c2 .v.c3 -fill both -side top
 
 set s [::Plotchart::createXYPlot .c {0.0 100.0 10.0} {0.0 100.0 20.0}]
+set r [::Plotchart::createRightAxis .c {0.0 0.1 0.01}]
 
 set xd    5.0
 set yd   20.0
@@ -58,7 +59,16 @@ $s interval series2 60.0 40.0 60.0
 
 $s xtext "X-coordinate"
 $s ytext "Y-data"
+$r ytext "Right axis"
 $s title "Aha!"
+
+#
+# Some data for the right axis
+#
+$r dataconfig right -type both -symbol circle -colour green
+$r plot right 10.0 0.01
+$r plot right 30.0 0.03
+$r plot right 40.0 0.02
 
 tkwait visibility .c
 $s saveplot "aha.ps"
