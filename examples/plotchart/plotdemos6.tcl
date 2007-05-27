682c2c5e8752bc2d0f53c1bf3b20da009907a316
Arjen Markus <arjen.markus@deltares.nl>
2007-05-27 08:17:55 +0000

    Added a demonstration of the dot subcommand

--- examples/plotchart/plotdemos6.tcl
+++ examples/plotchart/plotdemos6.tcl
@@ -19,7 +19,8 @@ package require Plotchart
 #
 #
 canvas .c  -background white -width 400 -height 400
-pack   .c -fill both -side top
+canvas .c2 -background white -width 400 -height 200
+pack   .c .c2 -fill both -side top
 
 set s [::Plotchart::createXYPlot .c {0.0 100.0 10.0} {0.0 100.0 20.0}]
 
@@ -48,3 +49,21 @@ foreach {length angle} $data {
    $s vector series2 $x $y $length $angle
 }
 
+set s2 [::Plotchart::createXYPlot .c2 {0.0 100.0 10.0} {0.0 100.0 20.0}]
+
+$s2 dotconfig series1 -colour "red" -scalebyvalue 1 -scale 2.5
+$s2 dotconfig series2 -colour "magenta" -classes {0 blue 1 green 2 yellow 3 red} \
+    -scalebyvalue 0 -outline 0
+$s2 dotconfig series3 -colour "magenta" -classes {0 blue 1 green 2 yellow 3 red} \
+    -scalebyvalue 1 -scale 2.5
+
+set y1 20
+set y2 50
+set y3 80
+set x  10
+foreach value {-1.0 0.5 1.5 2.5 3.5 4.5} {
+    $s2 dot series1 $x $y1 $value
+    $s2 dot series2 $x $y2 $value
+    $s2 dot series3 $x $y3 $value
+    set x [expr {$x + 10}]
+}
