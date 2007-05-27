e624c13a942c7bb60e34862d73648cd7563a2471
arjenmarkus <arjenmarkus>
2007-05-27 09:03:43 +0000

    Added dipole picture

--- examples/plotchart/plotdemos6.tcl
+++ examples/plotchart/plotdemos6.tcl
@@ -67,3 +67,48 @@ foreach value {-1.0 0.5 1.5 2.5 3.5 4.5} {
     $s2 dot series3 $x $y3 $value
     set x [expr {$x + 10}]
 }
+
+#
+# A more interesting vector plot: the forces in a dipole field
+#
+proc forcesDipole {x y} {
+    set xd1 51.0
+    set yd1 50.0
+    set xd2 49.0
+    set yd2 50.0
+
+    set r1p3 [expr {pow(hypot($x-$xd1,$y-$yd1),3.0)}]
+    set r2p3 [expr {pow(hypot($x-$xd2,$y-$yd2),3.0)}]
+
+    set fx [expr {($x-$xd1)/$r1p3 - ($x-$xd2)/$r2p3}]
+    set fy [expr {($y-$yd1)/$r1p3 - ($y-$yd2)/$r2p3}]
+
+    return [list $fx $fy]
+}
+
+toplevel .dipole
+canvas .dipole.c -background white -width 500 -height 500
+pack   .dipole.c -fill both -side top
+
+set s [::Plotchart::createXYPlot .dipole.c {45.0 55.0 1.0} {45.0 55.0 1.0}]
+
+$s title "Forces in a dipole field"
+
+$s vectorconfig series1 -colour "black" -scale 40 -type polar
+
+$s dotconfig dipole -colour red -scalebyvalue 0 -radius 5
+$s dot dipole 49.0 50.0 1.0
+$s dot dipole 51.0 50.0 1.0
+
+for {set y 45.25} {$y < 55.0} {set y [expr {$y+0.5}]} {
+    for {set x 45.25} {$x < 55.0} {set x [expr {$x+0.5}]} {
+        foreach {u v} [forcesDipole $x $y] {break}
+
+        # Scale the vector for better display
+
+        set angle  [expr {180.0*atan2($v,$u)/3.1415926}]
+        set length [expr {(0.5+hypot($u,$v))/(1.0+hypot($u,$v))}]
+
+        $s vector series1 $x $y $length $angle
+    }
+}
