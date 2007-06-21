ed93c52a6092447729baa9f6b8c880a035052aa9
hobbs <hobbs>
2007-06-21 00:08:09 +0000

    * toolbar.tcl (add): update to 1.2 	update option handling with latest snit features.

--- modules/widget/pkgIndex.tcl
+++ modules/widget/pkgIndex.tcl
@@ -25,5 +25,5 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {return}
     widget::scrolledwindow	1.2	scrollw.tcl
     widget::statusbar		1.1	statusbar.tcl
     widget::superframe		1.0	superframe.tcl
-    widget::toolbar		1.1	toolbar.tcl
+    widget::toolbar		1.2	toolbar.tcl
 }
