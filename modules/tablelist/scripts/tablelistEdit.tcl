a6315d6adace5a6548e7dcd09012bb2e776512ad
Csaba Nemethi <csaba.nemethi@t-online.de>
2008-04-27 18:40:22 +0000

    * CHANGES.txt:               Improved the support for cell editing with 	* scripts/tablelistEdit.tcl: a word- or char-wrapped text widget. 	* doc/tablelistTkCore.html:

--- modules/tablelist/scripts/tablelistEdit.tcl
+++ modules/tablelist/scripts/tablelistEdit.tcl
@@ -1351,23 +1351,26 @@ proc tablelist::doEditCell {win row col restore {cmd ""} {charPos -1}} {
 	destroy $f
 	return -code error $result
     }
+    catch {$w configure -relief ridge}
+    catch {$w configure -highlightthickness 0}
+    clearTakefocusOpt $w
     set class [winfo class $w]
     set isCheckbtn [string match "*Checkbutton" $class]
     set isText [expr {[string compare $class "Text"] == 0}]
     set isMentry [expr {[string compare $class "Mentry"] == 0}]
-    catch {$w configure -relief ridge}
-    catch {$w configure -highlightthickness 0}
     if {!$isCheckbtn} {
 	catch {$w configure -borderwidth 2}
+    }
+    if {$isText && $data($col-wrap) && $::tk_version >= 8.5} {
+	$w configure -wrap word
     }
     set alignment [lindex $data(colList) [expr {2*$col + 1}]]
     if {!$isText && !$isMentry} {
 	catch {$w configure -justify $alignment}
     }
-    clearTakefocusOpt $w
 
     #
-    # Replace the cell contents between the two tabs with the above frame
+    # Replace the cell's contents between the two tabs with the above frame
     #
     array set data [list editKey $key editRow $row editCol $col]
     findTabs $win [expr {$row + 1}] $col $col tabIdx1 tabIdx2
@@ -1447,27 +1450,6 @@ proc tablelist::doEditCell {win row col restore {cmd ""} {charPos -1}} {
 	    [eval [strMap {"%W" "$w"} $editWin($name-getTextCmd)]]
 	set data(rejected) 0
 
-	if {$isText} {
-	    #
-	    # Adjust the edit window's height
-	    #
-	    if {$data($col-wrap) && $::tk_version >= 8.5} {
-		$w configure -wrap word
-		bind $w <Configure> {
-		    set numLines [%W count -displaylines 1.0 end]
-		    %W configure -height $numLines
-		    [winfo parent %W] configure -height [winfo reqheight %W]
-		}
-	    } else {
-		scan [$w index end-1c] "%d" numLines
-		$w configure -height $numLines
-	    }
-	    if {[info exists ::wcb::version]} {
-		wcb::cbappend $w after insert tablelist::adjustTextHeight
-		wcb::cbappend $w after delete tablelist::adjustTextHeight
-	    }
-	}
-
 	if {[string compare $editWin($name-getListCmd) ""] != 0 &&
 	    [string compare $editWin($name-selectCmd) ""] != 0} {
 	    #
@@ -1529,6 +1511,26 @@ proc tablelist::doEditCell {win row col restore {cmd ""} {charPos -1}} {
 		$comp icursor end
 		$comp selection range 0 end
 	    }
+	}
+    }
+
+    if {$isText} {
+	#
+	# Adjust the edit window's height
+	#
+	if {[string compare [$w cget -wrap] "none"] != 0 &&
+	    $::tk_version >= 8.5} {
+	    bind $w <Configure> {
+		%W configure -height [%W count -displaylines 1.0 end]
+		[winfo parent %W] configure -height [winfo reqheight %W]
+	    }
+	} else {
+	    scan [$w index end-1c] "%d" numLines
+	    $w configure -height $numLines
+	}
+	if {[info exists ::wcb::version]} {
+	    wcb::cbappend $w after insert tablelist::adjustTextHeight
+	    wcb::cbappend $w after delete tablelist::adjustTextHeight
 	}
     }
 
