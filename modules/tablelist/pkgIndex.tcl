e219577fdee2910bcb9e3f4a94d2b0eab2c1d022
Csaba Nemethi <csaba.nemethi@t-online.de>
2008-04-27 19:33:41 +0000

    * *.tcl:         Bumped the version number to 4.9.1. 	* COPYRIGHT.txt:

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -7,22 +7,22 @@
 #
 # Regular packages:
 #
-package ifneeded tablelist         4.9 \
+package ifneeded tablelist         4.9.1 \
 	[list source [file join $dir tablelist.tcl]]
-package ifneeded tablelist_tile    4.9 \
+package ifneeded tablelist_tile    4.9.1 \
 	[list source [file join $dir tablelist_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Tablelist         4.9 \
-	[list package require -exact tablelist	    4.9]
-package ifneeded Tablelist_tile    4.9 \
-	[list package require -exact tablelist_tile 4.9]
+package ifneeded Tablelist         4.9.1 \
+	[list package require -exact tablelist	    4.9.1]
+package ifneeded Tablelist_tile    4.9.1 \
+	[list package require -exact tablelist_tile 4.9.1]
 
 #
 # Code common to all packages:
 #
-package ifneeded tablelist::common 4.9 \
+package ifneeded tablelist::common 4.9.1 \
         "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
 	 source [list [file join $dir tablelistPublic.tcl]]"
--- modules/tablelist/tablelistPublic.tcl
+++ modules/tablelist/tablelistPublic.tcl
@@ -8,7 +8,7 @@ namespace eval ::tablelist {
     #
     # Public variables:
     #
-    variable version	4.9
+    variable version	4.9.1
     variable library	[DIR]
 
     #
