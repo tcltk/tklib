73786bd287b495b2b6e12b3be521d0751f26699a
hobbs <hobbs>
2008-11-14 03:40:23 +0000

    * pkgIndex.tcl: widget::calendar 0.9 ; widget::dataentry 0.91 	* calendar.tcl: new widget::calendar adapted from the dateentry 	dropbox pieces.  Now can be used as a stand-alone widget. 	* dateentry.tcl: update to use calendar widget in dropbox.

--- modules/widget/pkgIndex.tcl
+++ modules/widget/pkgIndex.tcl
@@ -16,7 +16,8 @@ if {![llength [info commands ::tcl::pkgindex]]} {
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 ::tcl::pkgindex $dir widget::all 1.2 {
     widget			3.0	widget.tcl
-    widget::dateentry		0.9	dateentry.tcl
+    widget::calendar		0.9	calendar.tcl
+    widget::dateentry		0.91	dateentry.tcl
     widget::dialog		1.2	dialog.tcl
     widget::menuentry		1.0	mentry.tcl
     widget::panelframe		1.1	panelframe.tcl
