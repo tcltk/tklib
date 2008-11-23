13141b8a774541d360b72650e8d9396f1824803d
andreas_kupries <andreas_kupries>
2008-11-23 22:45:33 +0000

    Extended the canvas::sqmap example with zooming and filesystem based tile cache.

--- examples/canvas/osm.tcl
+++ examples/canvas/osm.tcl
@@ -5,16 +5,28 @@ exec tclsh "$0" ${1+"$@"}
 
 ## DEMO. Uses openstreetmap to show a tile-based world map.
 
+## Call without arguments for a plain web-served map.
+## Call with single argument (dir path) to use a tile cache.
+
+## Syntax: osm ?cachedir?
+
+## -- Note: The cache may not exist, it is automatically filled and/or
+##    extended from the web-served data. This cache can grow very
+##    large very quickly (I have currently seen ranging in size from
+##    4K (water) to 124K (dense urban area)).
+
 # ### ### ### ######### ######### #########
 ## Use canvas package relative to example location.
 
 set selfdir  [file dirname [file normalize [info script]]]
 set modules [file join [file dirname [file dirname $selfdir]] modules]
 
-source $modules/canvas/canvas_sqmap.tcl
-source $selfdir/tiles_xy_store_http.tcl
+source $modules/canvas/canvas_sqmap.tcl ; # The main map support
+source $selfdir/tiles_xy_store_http.tcl ; # Retrieving tiles from the web
+source $selfdir/osm_zoom.tcl            ; # Simple zoom-control (button based)
 
 ## Ideas:
+## == DONE ==
 ## -- Add zoom-control to switch between zoom levels. This has to
 ##    adjust the scroll-region as well. The control can be something
 ##    using basic Tk widgets (scale, button), or maybe some constructed
@@ -23,6 +35,7 @@ source $selfdir/tiles_xy_store_http.tcl
 ##    data out of the canvas::sqmap to move the item-group in sync
 ##    with scrolling, so that they appaear to stay in place.
 ##
+## == DONE ==
 ## -- Add a filesystem based tile cache to speed up their loading. The
 ##    pure http access is slow (*) OTOH, this makes the workings of
 ##    sqmap more observable, as things do not happen as fast as for
@@ -40,6 +53,9 @@ source $selfdir/tiles_xy_store_http.tcl
 ## -- Select two locations, then compute the geo distance between
 ##    them. Or, select a series of location, like following a road,
 ##    and compute the partial and total distances.
+
+## -- Mark, save, load series of points (gps tracks, own tracks).
+##    Name point series. Name individual points (location marks).
 
 # ### ### ### ######### ######### #########
 ## Other requirements for this example.
@@ -82,8 +98,9 @@ proc Goto {lat lon} {
     set ofy [expr {$fy - ($b - $t)/2.0/$scrollh}]
     set ofx [expr {$fx - ($r - $l)/2.0/$scrollw}]
 
-puts $lat\t$ty\t$y\t$fy\t$ofy
-puts $lon\t$tx\t$x\t$fx\t$ofx
+    # Show lat/lon, tile coord, pix coord, region fractionals
+    puts $lat\t$ty\t$y\t$fy\t$ofy
+    puts $lon\t$tx\t$x\t$fx\t$ofx
 
     .map xview moveto $ofx
     .map yview moveto $ofy
@@ -137,28 +154,134 @@ proc rad2deg {r} {
 
 # ### ### ### ######### ######### #########
 
-set location {}
+proc TS {level} {
+    global ts
+    if {![info exists ts($level)]} {
+	# Core tile provider, http to openstreetmap tile server.
+	# ---------------------------------------------------------
+	# OpenStreetMap. Mapnik rendered tiles.
 
-proc GUI {} {
-    global scrollw scrollh tc tr tile
+	set url  http://tile.openstreetmap.org
+	#set url  http://tah.openstreetmap.org/Tiles/tile
+	set ts($level) [tiles::xy::store::http TS/$level $level $url/$level]
+    }
+    return $ts($level)
+}
 
-    # ---------------------------------------------------------
-    # OpenStreetMap, Zoom Level 12 (of 18). Mapnik rendered tiles.
-    set zoom 12
-    set url  http://tile.openstreetmap.org
-    #set url  http://tah.openstreetmap.org/Tiles/tile
+proc Init {} {
+    global argv cachedir
+    set cachedir ""
 
-    tiles::xy::store::http TS $zoom $url/$zoom
+    # Nothing to do if no cache is specified
+    if {![llength $argv]} return
 
-    set tile     [TS tileheight] ; # assume quadratic cells.
-    set tc       [TS columns]
-    set tr       [TS rows]
+    # Fail for wrong#args
+    if {[llength $argv] > 1} Usage
+
+    set cachedir [lindex $argv 0]
+    return
+}
+
+proc Usage {} {
+    global argv0
+    puts stderr "wrong\#args, expected: $argv0 ?cachedir?"
+    exit 1
+}
+
+# ### ### ### ######### ######### #########
+## Manage a filesystem cache of tiles.
+
+## AK FUTURE: Put this into a generalized snit::type, i.e. for all
+## types of keys, like with cache::async. Then we can simply chain
+## objects to set everything up.
+
+proc Cache {__ at donecmd} {
+    global zoom cachedir tc tr
+
+    foreach {r c} $at break
+
+    if {
+	($r < 0) ||
+	($r >= $tr) ||
+	($c < 0) ||
+	($c >= $tc)
+    } {
+	puts "OUT OF BOUNDS ($at)"
+	after 0 [linsert $donecmd end unset $at]
+	return
+    }
+
+    if {$cachedir ne ""} {
+	puts "\tCache? ($at) -: $cachedir"
+
+	set tilefile [file join $cachedir $zoom $c $r.png]
+	if {[file exists $tilefile]} {
+	    puts "\t\tYES, serve immediately"
+	    set tile [image create photo -file $tilefile]
+	    after 0 [linsert $donecmd end set $at $tile]
+	    return
+	}
+    }
+
+    # Not in the cache, or no cache. Invoke the original provider and
+    # route results back to us so that we can extend the cache.
+
+    [TS $zoom] get $at [list CacheExtend $zoom $donecmd]
+    return
+}
+
+proc CacheExtend {z donecmd what at args} {
+    # note: z = zoom, at the time the request was made. In case the
+    # zoom level was changed by the user between start of the request
+    # and return of the result form the server.
+    global cachedir
+    puts "\tCacheExtend $z/ ($donecmd) $what ($at) $args"
+    if {$what eq "set"} {
+	# Enter the newly retrieved tile into the local cache
+	foreach {r c} $at break
+	set tile     [lindex $args 0]
+	set tilefile [file join $cachedir $z $c $r.png]
+	file mkdir [file dirname $tilefile]
+	$tile write $tilefile -format png
+    }
+    if {[catch {
+	uplevel #0 [eval [linsert $args 0 linsert $donecmd end $what $at]]
+    }]} { puts $::errorInfo }
+    return
+}
+
+proc ZOOM {w level} {
+    global zoom tile tc tr scrollw scrollh
+    puts Z=$level
+
+    set p [TS $level]
+
+    # Set up the data for map config and lat/lon conversion.
+    set tile     [$p tileheight] ; # assume quadratic cells.
+    set tc       [$p columns]
+    set tr       [$p rows]
     set scrollw  [expr {$tile * $tc}]
     set scrollh  [expr {$tile * $tr}]
 
-    puts r/[TS rows]
-    puts c/[TS columns]
+    puts r/$tr
+    puts c/$tc
 
+    # Tell Cache/provider
+    set zoom $level
+
+    # Reload map tiles
+    .map configure \
+	-grid-cell-width  $tile \
+	-grid-cell-height $tile \
+	-scrollregion [list 0 0 $scrollw $scrollh]
+    return
+}
+
+# ### ### ### ######### ######### #########
+
+set location {}
+
+proc GUI {} {
     # ---------------------------------------------------------
 
     widget::scrolledwindow .sw
@@ -168,7 +291,7 @@ proc GUI {} {
     button                 .grid -command ShowGrid -text {Show Grid}
     entry                  .loc  -textvariable location \
 	-bd 2 -relief sunken -bg white -width 80
-
+    zoom .z -orient vertical -levels 19 -variable ::zoom -command ZOOM
     .sw setwidget .map
 
     # Panning via mouse
@@ -183,27 +306,30 @@ proc GUI {} {
     if 1 {
 	# This routes the requests and results through GOT/GET logging
 	# commands.
-	.map configure \
-	    -grid-cell-command GET \
-	    -grid-cell-width  $tile \
-	    -grid-cell-height $tile \
-	    -scrollregion [list 0 0 $scrollw $scrollh]
+	.map configure -grid-cell-command GET
     } else {
 	# This routes the requests directly to the grid provider, and
 	# results back.
-	.map configure \
-	    -grid-cell-command TS \
-	    -grid-cell-width  $tile \
-	    -grid-cell-height $tile \
-	    -scrollregion [list 0 0 $scrollw $scrollh]
+	.map configure -grid-cell-command Cache
     }
 
-    pack .sw   -expand 1 -fill both -side bottom
-    pack .exit -expand 0 -fill both -side left
-    pack .ac   -expand 0 -fill both -side left
-    pack .grid   -expand 0 -fill both -side left
-    pack .loc  -expand 0 -fill both -side left
+    grid .z    -row 1 -column 0 -sticky wen
+    grid .sw   -row 1 -column 1 -sticky swen -columnspan 4
+    grid .exit -row 0 -column 1 -sticky wen
+    grid .ac   -row 0 -column 2 -sticky wen
+    grid .grid -row 0 -column 3 -sticky wen
+    grid .loc  -row 0 -column 4 -sticky wen
 
+    grid rowconfigure . 0 -weight 0
+    grid rowconfigure . 1 -weight 1
+
+    grid columnconfigure . 0 -weight 0
+    grid columnconfigure . 1 -weight 0
+    grid columnconfigure . 2 -weight 0
+    grid columnconfigure . 3 -weight 0
+    grid columnconfigure . 4 -weight 1
+
+    ZOOM . 0
     return
 }
 
@@ -226,7 +352,7 @@ proc Aachen {} {
 
 proc GET {__ at donecmd} {
     puts "GET ($at) ($donecmd)"
-    TS get $at [list GOT $donecmd]
+    Cache get $at [list GOT $donecmd]
     return
 }
 
@@ -263,6 +389,8 @@ proc VPTRACK {xl yt xr yb} {
 # ### ### ### ######### ######### #########
 ## Basic interface.
 
+Init
 GUI
 # banff       lat = 51.1653,  lon = -115.5322
 # lost lagoon lat = 49.30198, lon = -123.13724
+after 1000 {ZOOM . 12 ; after 10 Aachen}
