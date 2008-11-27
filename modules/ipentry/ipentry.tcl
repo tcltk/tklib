e595c512b8b4fdec7c1582826786266af1d7b7c8
afaupell <afaupell>
2008-11-27 02:32:13 +0000

    add comments

--- modules/ipentry/ipentry.tcl
+++ modules/ipentry/ipentry.tcl
@@ -7,16 +7,18 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: ipentry.tcl,v 1.10 2006/07/15 02:22:53 afaupell Exp $
+# RCS: @(#) $Id: ipentry.tcl,v 1.11 2008/11/27 02:32:13 afaupell Exp $
 
 package require Tk
 package provide ipentry 0.2
 
 namespace eval ::ipentry {
     namespace export ipentry
+    # copy all the bindings from Entry class to our own IPEntrybindtag class
     foreach x [bind Entry] {
         bind IPEntrybindtag $x [bind Entry $x]
     }
+    # then replace certain keys we are interested in with our own
     bind IPEntrybindtag <KeyPress>         {::ipentry::keypress %W %K}
     bind IPEntrybindtag <BackSpace>        {::ipentry::backspace %W}
     bind IPEntrybindtag <period>           {::ipentry::dot %W}
@@ -29,6 +31,18 @@ namespace eval ::ipentry {
     bind IPEntrybindtag <Key-Tab>          {::ipentry::tab %W; break}
 }
 
+# ipentry --
+#
+# main entry point - construct a new ipentry widget
+#
+# ARGS:
+#       w       path name of widget to create
+#
+#               see ::ipentry::configure for args
+#
+# RETURNS:
+#       the widget path name
+#
 proc ::ipentry::ipentry {w args} {
     frame $w -bd 2 -relief sunken -class IPEntry
     foreach x {0 1 2 3} y {d1 d2 d3 d4} {
@@ -42,6 +56,7 @@ proc ::ipentry::ipentry {w args} {
     }
     destroy $w.d4
     rename ::$w ::ipentry::_$w
+    # redirect the widget name command to the widgetCommand dispatcher
     interp alias {} ::$w {} ::ipentry::widgetCommand $w
     namespace eval _tvns$w {variable textvarname}
     bind $w <Destroy> [list ::ipentry::destroyWidget $w]
@@ -52,10 +67,24 @@ proc ::ipentry::ipentry {w args} {
     return $w
 }
 
+# keypress --
+#
+# called every time a key is pressed in an ipentry widget
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#       key     the keysym (%K) from the event
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::keypress {w key} {
     if {![validate $w $key]} { return }
+    # sel.first and sel.last throw an error if the selection isnt in $w
     catch {
         set insert [$w index insert]
+        # if a key is pressed while there is a selection then delete the
+        # selected chars
         if {([$w index sel.first] <= $insert) && ([$w index sel.last] >= $insert)} {
             $w delete sel.first sel.last
         }
@@ -63,10 +92,35 @@ proc ::ipentry::keypress {w key} {
     $w insert insert $key
 }
 
+# tab --
+#
+# called when the Tab key is pressed in an ipentry widget
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::tab {w} {
+    # redirect to the standard tk handler but use the parent frame instead
+    # of the entry widget
     tk::TabToWindow [tk_focusNext [winfo parent $w].3]
 }
 
+# backspace --
+#
+# called when the Backspace key is pressed in an ipentry widget
+#
+# try to act like a normal backspace except if the cursor is at index 0
+# of one entry we need to move to the end of the preceding entry
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::backspace {w} {
     if {[$w selection present]} {
         $w delete sel.first sel.last
@@ -79,12 +133,36 @@ proc ::ipentry::backspace {w} {
     }
 }
 
+# dot --
+#
+# called when the dot (Period) key is pressed in an ipentry widget
+#
+# treat the current entry as done and move to the next entry widget
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::dot {w} {
     if {[string length [$w get]] > 0} {
         skip $w next 1
     }
 }
 
+# FocusIn --
+#
+# called when the focus enters any of the child widgets of an ipentry
+#
+# clear the selection of all child widgets other than the one with focus
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::FocusIn {w} {
     set p [winfo parent $w]
     foreach x {0 1 2 3} {
@@ -94,6 +172,18 @@ proc ::ipentry::FocusIn {w} {
     }
 }
 
+# FocusOut --
+#
+# called when the focus leaves any of the child widgets of an ipentry
+#
+# 
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::FocusOut {w} {
     set s [$w get]
     if {[string match {*.0} $w] && $s != "" && $s < 1} {
@@ -102,18 +192,35 @@ proc ::ipentry::FocusOut {w} {
     }
 }
 
+# Paste --
+#
+# called from the <<Paste>> virtual event
+#
+# clear the selection of all child widgets other than the one with focus
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#       sel     one of CLIPBOARD or PRIMARY
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::Paste {w sel} {
     if {[catch {::tk::GetSelection $w $sel} paste]} { return }
     $w delete 0 end
     foreach char [split $paste {}] {
+        # ignore everything except dots and digits
         if {![string match {[0123456789.]} $char]} { continue }
         if {$char != "."} {
             $w insert end $char
         }
+        # if value is over 255 truncate it
         if {[$w get] > 255} {
             $w delete 0 end
             $w insert 0 255
         }
+        # if char is a . then get the index of the current entry
+        # and update $w to point to the next entry
         if {$char == "."} {
             set n [string index $w end]
             if { $n >= 3 } { return }
@@ -124,6 +231,19 @@ proc ::ipentry::Paste {w sel} {
     }
 }
 
+# dotclick --
+#
+# called when mouse button 1 is clicked on any of the label widgets
+#
+# decide which side of the dot was clicked and put the focus and cursor
+# in the correct entry
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::dotclick {w x} {
     if {$x > ([winfo width $w] / 2)} {
         set w [winfo parent $w].[string index $w end]
@@ -136,11 +256,24 @@ proc ::ipentry::dotclick {w x} {
     }
 }
 
+# arrow --
+#
+# called when the left or right arrow keys are pressed in an ipentry
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#       key     one of Left or Right
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::arrow {w key} {
     set i [$w index insert]
     set l [string length [$w get]]
+    # move the icursor +1 or -1 position
     $w icursor [expr $i [string map {Right + Left -} $key] 1]
     $w selection clear
+    # if we are moving right and the cursor is at the end, or the entry is empty
     if {$key == "Right" && ($i == $l || $l == 0)} {
         skip $w next
     } elseif {$key == "Left" && $i == 0} {
@@ -148,30 +281,54 @@ proc ::ipentry::arrow {w key} {
     }
 }
 
+# validate --
+#
+# called by keypress to validate the input
+#
+# ARGS:
+#       w       window argument (%W) from the event binding
+#       key     the key pressed
+#
+# RETURNS:
+#       a boolean indicating if the key is valid or not
+#
 proc ::ipentry::validate {w key} {
     if {![string match {[0123456789]} $key]} { return 0 }
-    set s [$w get]
-    set i [$w index insert]
-    if {$s == "0" && $key == "0"} { return 0 }
-    if {[string length $s] == 2} {
-        set s [join [linsert [split $s {}] $i $key] {}]
-        if {$s > 255} {
+    set curval [$w get]
+    set insert [$w index insert]
+    # dont allow more than a single 0 to be entered
+    if {$curval == "0" && $key == "0"} { return 0 }
+    if {[string length $curval] == 2} {
+        set curval [join [linsert [split $curval {}] $insert $key] {}]
+        if {$curval > 255} {
             $w delete 0 end
             $w insert 0 255
             $w selection range 0 end
             return 0
-        } elseif {$i == 2} {
+        } elseif {$insert == 2} {
             skip $w next 1
         }
         return 1
     }
-    if {[string length $s] >= 3 && ![$w selection present]} {
-        if {$i == 3} { skip $w next 1 }
+    if {[string length $curval] >= 3 && ![$w selection present]} {
+        if {$insert == 3} { skip $w next 1 }
         return 0
     }
     return 1
 }
 
+# skip --
+#
+# move the cursor to the previous or next entry widget
+#
+# ARGS:
+#       w       name of the current entry widget 
+#       dir     direction to move, one of next or prev
+#       sel     boolean indicating whether to select the digits in the next entry
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::skip {w dir {sel 0}} {
     set n [string index $w end]
     if {$dir == "next"} {
@@ -190,16 +347,40 @@ proc ::ipentry::skip {w dir {sel 0}} {
     }
 }
 
+# _foreach --
+#
+# perform a command on every subwidget of an ipentry frame
+#
+# ARGS:
+#       w       name of the ipentry frame 
+#       cmd     command to perform
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::_foreach {w cmd} {
     foreach x {0 d1 1 d2 2 d3 3} {
         eval [list $w.$x] $cmd
     }
 }
 
+# cget --
+#
+# handle the widgetName cget subcommand
+#
+# ARGS:
+#       w       name of the ipentry widget 
+#       cmd     name of a configuration option
+#
+# RETURNS:
+#       the value of the requested option
+#
 proc ::ipentry::cget {w cmd} {
     switch -exact -- $cmd {
-        -bd     -
+        -bd -
+        -borderwidth -
         -relief {
+            # for bd and relief return the value from the container frame
             return [::ipentry::_$w cget $cmd]
         }
         -textvariable {
@@ -212,12 +393,24 @@ proc ::ipentry::cget {w cmd} {
             }
         }
         default {
+            # for all other commands return the value from the first entry
             return [$w.0 cget $cmd]
         }
     }
     return
 }
 
+# configure --
+#
+# handle the widgetName configure subcommand
+#
+# ARGS:
+#       w       name of the ipentry widget 
+#       args    name/value pairs of configuration options
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::configure {w args} {
     while {[set cmd [lindex $args 0]] != ""} {
         switch -exact -- $cmd {
@@ -302,6 +495,17 @@ proc ::ipentry::configure {w args} {
     }
 }
 
+# configure --
+#
+# handle the widgetName configure subcommand
+#
+# ARGS:
+#       w       name of the ipentry widget 
+#       args    name/value pairs of configuration options
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::destroyWidget {w} {
     upvar #0 [$w cget -textvariable] var
     trace remove variable var [list array read write unset] \
@@ -310,6 +514,19 @@ proc ::ipentry::destroyWidget {w} {
     rename $w {}
 }
 
+# traceVar --
+#
+# called by the variable trace for the ipentry textvariable
+#
+# ARGS:
+#       w       name of the ipentry widget 
+#       varname name of the variable being traced
+#       key     array index of the variable
+#       op      operation performed on the variable, read/write/unset
+#
+# RETURNS:
+#       nothing
+#
 proc ::ipentry::traceVar {w varname key op} {
     upvar #0 $varname var
 
@@ -343,6 +560,17 @@ proc ::ipentry::traceVar {w varname key op} {
 
 }
 
+# isValid --
+#
+# determine if val is a valid ip address
+# used by the textvariable routines
+#
+# ARGS:
+#       val     value to test
+#
+# RETURNS:
+#       boolean indicating if val is a valid ip address
+#
 proc ::ipentry::isValid {val} {
     set lval [split [join $val] {. }]
     set valid 1
@@ -350,19 +578,30 @@ proc ::ipentry::isValid {val} {
         set valid 0
     } else {
         foreach n $lval {
-            if { $n == "" || ![string is integer -strict $n]
-              || $n > 255 || $n < 0 } {
-                set valid 0
-                break
-            }
+        if { $n == "" || ![string is integer -strict $n] || $n > 255 || $n < 0 } {
+            set valid 0
+            break
         }
     }
     return $valid
 }
 
+# widgetCommand --
+#
+# handle the widgetName command
+#
+# ARGS:
+#       w       name of the ipentry widget 
+#       cmd     the subcommand
+#       args    arguments to the subcommand
+#
+# RETURNS:
+#       the results of the invoked subcommand
+#
 proc ::ipentry::widgetCommand {w cmd args} {
     switch -exact -- $cmd {
         get {
+            # return the 4 entry values as a list
             foreach x {0 1 2 3} {
                 set s [$w.$x get]
                 if {[string length $s] > 1} { set s [string trimleft $s 0] }
