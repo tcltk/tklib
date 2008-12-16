8f57f75cb8e32ed092141968cdc5adfa5c2ce9e0
Arjen Markus <arjen.markus@deltares.nl>
2008-12-16 05:13:17 +0000

    Added scrolling for timecharts and Gantt charts; added boxplots; version is now 1.6.0

--- modules/plotchart/pkgIndex.tcl
+++ modules/plotchart/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded Plotchart 1.5.1 [list source [file join $dir plotchart.tcl]]
+package ifneeded Plotchart 1.6.0 [list source [file join $dir plotchart.tcl]]
