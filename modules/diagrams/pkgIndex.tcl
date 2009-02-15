2644845fe87f3843070f9604de0e8d21223ea420
Arjen Markus <arjen.markus@deltares.nl>
2009-02-15 13:35:59 +0000

    Updated the Diagrams package to version 0.3 (solving a few nagging bugs)

--- modules/diagrams/pkgIndex.tcl
+++ modules/diagrams/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.3]} {
     # PRAGMA: returnok
     return
 }
-package ifneeded Diagrams 0.2 [list source [file join $dir draw_diagram.tcl]]
+package ifneeded Diagrams 0.3 [list source [file join $dir draw_diagram.tcl]]
