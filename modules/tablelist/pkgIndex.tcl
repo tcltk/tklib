d0b689fbc7b408f3e25d591ebb7237547ae6f2ab
Csaba Nemethi <csaba.nemethi@t-online.de>
2009-02-23 21:38:50 +0000

    * *.tcl:              Bumped the version number to 4.11. 	* *.txt: 	* doc/tablelist.html:

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -7,22 +7,22 @@
 #
 # Regular packages:
 #
-package ifneeded tablelist         4.10.1 \
+package ifneeded tablelist         4.11 \
 	[list source [file join $dir tablelist.tcl]]
-package ifneeded tablelist_tile    4.10.1 \
+package ifneeded tablelist_tile    4.11 \
 	[list source [file join $dir tablelist_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Tablelist         4.10.1 \
-	[list package require -exact tablelist	    4.10.1]
-package ifneeded Tablelist_tile    4.10.1 \
-	[list package require -exact tablelist_tile 4.10.1]
+package ifneeded Tablelist         4.11 \
+	[list package require -exact tablelist	    4.11]
+package ifneeded Tablelist_tile    4.11 \
+	[list package require -exact tablelist_tile 4.11]
 
 #
 # Code common to all packages:
 #
-package ifneeded tablelist::common 4.10.1 \
+package ifneeded tablelist::common 4.11 \
         "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
 	 source [list [file join $dir tablelistPublic.tcl]]"
