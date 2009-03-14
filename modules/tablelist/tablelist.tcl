779f31ff92531604102301907a70fe71cb648a72
Csaba Nemethi <csaba.nemethi@t-online.de>
2009-03-14 21:47:19 +0000

    * *.tcl:         Bumped the version number to 4.11.1. 	* COPYRIGHT.txt:

--- modules/tablelist/tablelist.tcl
+++ modules/tablelist/tablelist.tcl
@@ -6,7 +6,7 @@
 
 package require Tcl 8
 package require Tk  8
-package require -exact tablelist::common 4.11
+package require -exact tablelist::common 4.11.1
 
 package provide tablelist $::tablelist::version
 package provide Tablelist $::tablelist::version
