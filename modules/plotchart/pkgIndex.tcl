66c1223bbd582e9b89888239081a7c8e6bd1c5a8
arjenmarkus <arjenmarkus>
2009-12-10 12:00:53 +0000

    Adding Jos Decoster's xyplot to the plotchart module. No documentation yet, but there is an example of how to use it.

--- modules/plotchart/pkgIndex.tcl
+++ modules/plotchart/pkgIndex.tcl
@@ -3,3 +3,4 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {
     return
 }
 package ifneeded Plotchart 1.8.2 [list source [file join $dir plotchart.tcl]]
+package ifneeded xyplot    1.0.0 [list source [file join $dir xyplot.tcl]]
