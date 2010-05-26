019051abae1555c171cd5d847ad896b4d667cbb0
andreas_kupries <andreas_kupries>
2010-05-26 20:02:31 +0000

    * canvas_snap.tcl: New package, taking snapshots of 	* canvas_snap.man: Canvases (to Tk photo image). 	* pkgIndex.tcl: Original author George Petasis, see 	  http://wiki.tcl.tk/1404.

--- modules/canvas/pkgIndex.tcl
+++ modules/canvas/pkgIndex.tcl
@@ -1,3 +1,5 @@
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded canvas::sqmap 0.2 [list source [file join $dir canvas_sqmap.tcl]]
 package ifneeded canvas::zoom  0.1 [list source [file join $dir canvas_zoom.tcl]]
+if {![package vsatisfies [package provide Tcl] 8.5]} { return }
+package ifneeded canvas::snap 1 [list source [file join $dir canvas_snap.tcl]]
