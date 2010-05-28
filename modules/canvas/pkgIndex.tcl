e2f774b84fc2785d876b79b518c81b272d936fd2
Andreas Kupries <akupries@shaw.ca>
2010-05-28 19:29:12 +0000

    * canvas_snap.tcl: Fixed oops, missing setup of our 	* canvas_snap.man: namespace. Version bumped to 1.0.1 	* pkgIndex.tcl: Thanks to George Petasis for noticing.

--- modules/canvas/pkgIndex.tcl
+++ modules/canvas/pkgIndex.tcl
@@ -2,4 +2,4 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded canvas::sqmap 0.2 [list source [file join $dir canvas_sqmap.tcl]]
 package ifneeded canvas::zoom  0.1 [list source [file join $dir canvas_zoom.tcl]]
 if {![package vsatisfies [package provide Tcl] 8.5]} { return }
-package ifneeded canvas::snap 1 [list source [file join $dir canvas_snap.tcl]]
+package ifneeded canvas::snap 1.0.1 [list source [file join $dir canvas_snap.tcl]]
