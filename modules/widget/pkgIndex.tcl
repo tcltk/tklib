3e300d9bc6c0543d3c50f22a5ea241d1fb19815a
Andreas Kupries <akupries@shaw.ca>
2010-06-01 20:02:10 +0000

    * pkgIndex.tcl: Updated with the actual package versions.

--- modules/widget/pkgIndex.tcl
+++ modules/widget/pkgIndex.tcl
@@ -15,17 +15,17 @@ if {![llength [info commands ::tcl::pkgindex]]} {
 }
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 ::tcl::pkgindex $dir widget::all 1.2 {
-    widget			3.0	widget.tcl
+    widget			3.1	widget.tcl
     widget::calendar		0.94	calendar.tcl
-    widget::dateentry		0.91	dateentry.tcl
-    widget::dialog		1.3	dialog.tcl
-    widget::menuentry		1.0	mentry.tcl
+    widget::dateentry		0.92	dateentry.tcl
+    widget::dialog		1.3.1	dialog.tcl
+    widget::menuentry		1.0.1	mentry.tcl
     widget::panelframe		1.1	panelframe.tcl
     widget::ruler		1.1	ruler.tcl
-    widget::screenruler		1.2	ruler.tcl
+    widget::screenruler		1.2.1	ruler.tcl
     widget::scrolledtext	1.0	stext.tcl
     widget::scrolledwindow	1.2	scrollw.tcl
-    widget::statusbar		1.2	statusbar.tcl
-    widget::superframe		1.0	superframe.tcl
-    widget::toolbar		1.2	toolbar.tcl
+    widget::statusbar		1.2.1	statusbar.tcl
+    widget::superframe		1.0.1	superframe.tcl
+    widget::toolbar		1.2.1	toolbar.tcl
 }
