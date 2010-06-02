846e364c4269b80583779ff076efe66cbb528b06
Andreas Kupries <andreas.kupries@gmail.com>
2010-06-02 15:27:24 +0000

    * pkgIndex.tcl: Meh, typo'd my update. screenruler is unchanged, 	  scrolledwindow isn't. Fixed.

--- modules/widget/pkgIndex.tcl
+++ modules/widget/pkgIndex.tcl
@@ -14,7 +14,7 @@ if {![llength [info commands ::tcl::pkgindex]]} {
     }
 }
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-::tcl::pkgindex $dir widget::all 1.2 {
+::tcl::pkgindex $dir widget::all 1.2.1 {
     widget			3.1	widget.tcl
     widget::calendar		0.94	calendar.tcl
     widget::dateentry		0.92	dateentry.tcl
@@ -22,9 +22,9 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {return}
     widget::menuentry		1.0.1	mentry.tcl
     widget::panelframe		1.1	panelframe.tcl
     widget::ruler		1.1	ruler.tcl
-    widget::screenruler		1.2.1	ruler.tcl
+    widget::screenruler		1.2	ruler.tcl
     widget::scrolledtext	1.0	stext.tcl
-    widget::scrolledwindow	1.2	scrollw.tcl
+    widget::scrolledwindow	1.2.1	scrollw.tcl
     widget::statusbar		1.2.1	statusbar.tcl
     widget::superframe		1.0.1	superframe.tcl
     widget::toolbar		1.2.1	toolbar.tcl
