dba5faaba9f88276a0c5a9384e709681a0a75e0c
Andreas Kupries <akupries@shaw.ca>
2010-06-08 18:09:31 +0000

    * pkgIndex.tcl: Added a guard command to xyplot, this package 	  requires Tcl 8.5+.

--- modules/plotchart/pkgIndex.tcl
+++ modules/plotchart/pkgIndex.tcl
@@ -3,4 +3,8 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {
     return
 }
 package ifneeded Plotchart 1.8.3 [list source [file join $dir plotchart.tcl]]
-package ifneeded xyplot    1.0.1 [list source [file join $dir xyplot.tcl]]
+if {![package vsatisfies [package provide Tcl] 8.5]} {
+    # PRAGMA: returnok
+    return
+}
+package ifneeded xyplot 1.0.1 [list source [file join $dir xyplot.tcl]]
