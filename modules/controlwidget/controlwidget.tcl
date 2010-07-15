bbad39cc3d15e0a56afcb34a3345d6f2df60cc79
Arjen Markus <arjen.markus@deltares.nl>
2010-07-15 10:42:37 +0000

    Started writing the manual for the various widgets. Improved implementation of several control widgets. Added the voltmeter widget and the matrix of radio buttons

---
+++ modules/controlwidget/controlwidget.tcl
@@ -0,0 +1,15 @@
+# controlwidget.tcl --
+#     Set up the requirements for the controlwidget module/package
+#     and source the individual files
+#
+
+package require Tk 8.5
+package require snit
+
+source [file join [file dirname [info script]] vertical_meter.tcl]
+source [file join [file dirname [info script]] led.tcl]
+source [file join [file dirname [info script]] rdial.tcl]
+source [file join [file dirname [info script]] tachometer.tcl]
+source [file join [file dirname [info script]] voltmeter.tcl]
+source [file join [file dirname [info script]] radioMatrix.tcl]
+source [file join [file dirname [info script]] bindDown.tcl]
