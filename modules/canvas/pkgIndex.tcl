f5fcbd9c392321992a923b0bb40889029ae2a09b
Andreas Kupries <andreas.kupries@gmail.com>
2010-07-20 02:21:13 +0000

    * canvas_sqmap.tcl: Extend the class with two option to specify 	* canvas_sqmap.man: images to show while the image for a cell is 	* pkgIndex.tcl: loaded, and in cells for which there is no 	  image. Updated the documentation. Bumped the version to 0.3.

--- modules/canvas/pkgIndex.tcl
+++ modules/canvas/pkgIndex.tcl
@@ -1,5 +1,5 @@
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded canvas::sqmap 0.2 [list source [file join $dir canvas_sqmap.tcl]]
+package ifneeded canvas::sqmap 0.3 [list source [file join $dir canvas_sqmap.tcl]]
 package ifneeded canvas::zoom  0.2 [list source [file join $dir canvas_zoom.tcl]]
 if {![package vsatisfies [package provide Tcl] 8.5]} { return }
 package ifneeded canvas::snap 1.0.1 [list source [file join $dir canvas_snap.tcl]]
