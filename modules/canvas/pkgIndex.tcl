995ea7d108c9e2e8a239add949895e377b49f8b3
Andreas Kupries <akupries@shaw.ca>
2010-08-27 03:19:27 +0000

    * canvas_mvg.tcl: New package. Partial conversion of canvas 	* canvas_mvg.man: contents to ImageMagick's MVG vector format. 	* pkgIndex.tcl:

--- modules/canvas/pkgIndex.tcl
+++ modules/canvas/pkgIndex.tcl
@@ -3,3 +3,5 @@ package ifneeded canvas::sqmap 0.3.1 [list source [file join $dir canvas_sqmap.t
 package ifneeded canvas::zoom  0.2.1 [list source [file join $dir canvas_zoom.tcl]]
 if {![package vsatisfies [package provide Tcl] 8.5]} { return }
 package ifneeded canvas::snap 1.0.1 [list source [file join $dir canvas_snap.tcl]]
+package ifneeded canvas::mvg  1     [list source [file join $dir canvas_mvg.tcl]]
+
