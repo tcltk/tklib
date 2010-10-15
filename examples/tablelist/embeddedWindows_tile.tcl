e19ab5003611624e80019a5e6c438b4f573b16e4
Csaba Nemethi <csaba.nemethi@t-online.de>
2010-10-15 21:16:41 +0000

    * *.tcl:                          Bumped the version number to 5.2. 	* *.txt: 	* ../../examples/tablelist/*.tcl:

--- examples/tablelist/embeddedWindows_tile.tcl
+++ examples/tablelist/embeddedWindows_tile.tcl
@@ -8,7 +8,7 @@ exec wish "$0" ${1+"$@"}
 # Copyright (c) 2004-2010  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package require tablelist_tile 5.1
+package require tablelist_tile 5.2
 
 wm title . "Tile Library Scripts"
 
