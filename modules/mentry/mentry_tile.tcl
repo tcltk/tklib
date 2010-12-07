9f218f86bfad2d3d1d50b76ad22074e40f561d7b
Csaba Nemethi <csaba.nemethi@t-online.de>
2010-12-07 19:58:39 +0000

    * Added mentry to tklib.

---
+++ modules/mentry/mentry_tile.tcl
@@ -0,0 +1,25 @@
+#==============================================================================
+# Main Mentry_tile package module.
+#
+# Copyright (c) 1999-2010  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+#==============================================================================
+
+package require Tcl 8.4
+package require Tk  8.4
+if {$::tk_version < 8.5 || [regexp {^8\.5a[1-5]$} $::tk_patchLevel]} {
+    package require tile 0.6
+}
+package require -exact mentry::common 3.4
+
+package provide Mentry_tile $::mentry::version
+package provide mentry_tile $::mentry::version
+
+::mentry::useTile 1
+::mentry::createBindings
+
+namespace eval ::mentry {
+    #
+    # Commands related to tile themes:
+    #
+    namespace export	getThemes getCurrentTheme setTheme setThemeDefaults
+}
