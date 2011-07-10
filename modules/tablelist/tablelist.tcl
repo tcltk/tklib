e1d4a87ed62655287617d5ce1958d20dc32c1d68
Csaba Nemethi <csaba.nemethi@t-online.de>
2011-07-10 20:29:05 +0000

    * *.tcl:              Bumped the version number to 5.3.1. 	* *.txt: 	* doc/tablelist.html:

--- modules/tablelist/tablelist.tcl
+++ modules/tablelist/tablelist.tcl
@@ -6,7 +6,7 @@
 
 package require Tcl 8
 package require Tk  8
-package require -exact tablelist::common 5.3
+package require -exact tablelist::common 5.3.1
 
 package provide tablelist $::tablelist::version
 package provide Tablelist $::tablelist::version
