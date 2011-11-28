cc517280e0c30daf917b94470abaa83b21629ca1
Csaba Nemethi <csaba.nemethi@t-online.de>
2011-11-28 21:09:50 +0000

    * *.tcl: Bumped the version number to 5.5. 	* *.txt:

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -7,22 +7,22 @@
 #
 # Regular packages:
 #
-package ifneeded tablelist         5.4 \
+package ifneeded tablelist         5.5 \
 	[list source [file join $dir tablelist.tcl]]
-package ifneeded tablelist_tile    5.4 \
+package ifneeded tablelist_tile    5.5 \
 	[list source [file join $dir tablelist_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Tablelist         5.4 \
-	[list package require -exact tablelist	    5.4]
-package ifneeded Tablelist_tile    5.4 \
-	[list package require -exact tablelist_tile 5.4]
+package ifneeded Tablelist         5.5 \
+	[list package require -exact tablelist	    5.5]
+package ifneeded Tablelist_tile    5.5 \
+	[list package require -exact tablelist_tile 5.5]
 
 #
 # Code common to all packages:
 #
-package ifneeded tablelist::common 5.4 \
+package ifneeded tablelist::common 5.5 \
         "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
 	 source [list [file join $dir tablelistPublic.tcl]]"
