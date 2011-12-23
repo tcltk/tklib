187b3ec82749b142da0be8e29852540662cd40f7
Arjen Markus <arjen.markus@deltares.nl>
2011-12-23 08:51:49 +0000

    Added several small examples/tests to show the new functionality of Plotchart`

---
+++ examples/plotchart/testfunc.tcl
@@ -0,0 +1,27 @@
+# testfunc.tcl --
+#     Test the plotfunc method
+#
+source plotchart.tcl
+package require Plotchart
+
+grid [canvas .c -width 500 -height 500]
+
+set p [::Plotchart::createXYPlot .c {-4.0 4.0 2.0} {-4.0 4.0 2.0}]
+
+proc parabola {a x} {
+    expr {$a**2 - $x**2}
+}
+proc semicircle {a x} {
+    expr {sqrt($a**2 - $x**2)}
+}
+proc hyperbola {a x} {
+    expr {sqrt($x**2 - $a**2)}
+}
+
+$p plotfunc parabola {x a} {[parabola $a $x]} 2.0
+
+$p dataconfig semicircle -colour red -width 2
+$p plotfunc semicircle {x a} {[semicircle $a $x]} 2.0 -samples 100
+
+$p dataconfig hyperbola -colour blue -width 2
+$p plotfunc hyperbola {x a} {[hyperbola $a $x]} 1.0 -samples 100
