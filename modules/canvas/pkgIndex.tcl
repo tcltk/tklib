69fc8cf94a96821932f735ad85d7863c7f76cf41
Andreas Kupries <akupries@shaw.ca>
2012-02-16 05:36:13 +0000

    * canvas_drag.man: Three new utility packages to help with 	* canvas_drag.tcl: canvas tag lists, highlighting, and dragging. 	* canvas_highlight.man: Plus an example application showing off 	* canvas_highlight.tcl: all three. 	* canvas_tags.man: 	* canvas_tags.tcl: 	* pkgIndex.tcl: 	* ../../examples/canvas/demo_draghigh.tcl

--- modules/canvas/pkgIndex.tcl
+++ modules/canvas/pkgIndex.tcl
@@ -2,6 +2,8 @@ if {![package vsatisfies [package provide Tcl] 8.4]} {return}
 package ifneeded canvas::sqmap 0.3.1 [list source [file join $dir canvas_sqmap.tcl]]
 package ifneeded canvas::zoom  0.2.1 [list source [file join $dir canvas_zoom.tcl]]
 if {![package vsatisfies [package provide Tcl] 8.5]} { return }
-package ifneeded canvas::snap 1.0.1 [list source [file join $dir canvas_snap.tcl]]
-package ifneeded canvas::mvg  1     [list source [file join $dir canvas_mvg.tcl]]
-
+package ifneeded canvas::snap      1.0.1 [list source [file join $dir canvas_snap.tcl]]
+package ifneeded canvas::mvg       1     [list source [file join $dir canvas_mvg.tcl]]
+package ifneeded canvas::tag       0.1   [list source [file join $dir canvas_tags.tcl]]
+package ifneeded canvas::drag      0.1   [list source [file join $dir canvas_drag.tcl]]
+package ifneeded canvas::highlight 0.1   [list source [file join $dir canvas_highlight.tcl]]
