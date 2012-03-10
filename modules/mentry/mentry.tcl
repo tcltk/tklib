616b5c0a4cae65acb21fb2385efe2e7243301fe3
nemethi <nemethi>
2012-03-10 19:16:00 +0000

    * *.tcl:         Bumped the version number to 3.6; updated the 	* COPYRIGHT.txt: copyright information. 	* README.txt:

--- modules/mentry/mentry.tcl
+++ modules/mentry/mentry.tcl
@@ -1,10 +1,10 @@
 #==============================================================================
 # Main Mentry package module.
 #
-# Copyright (c) 1999-2011  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 1999-2012  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package require -exact mentry::common 3.5
+package require -exact mentry::common 3.6
 
 package provide Mentry $::mentry::version
 package provide mentry $::mentry::version
