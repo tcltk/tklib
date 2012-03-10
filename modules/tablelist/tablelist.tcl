0ce53accf31da4873496baeb97051ad031bf4a02
Csaba Nemethi <csaba.nemethi@t-online.de>
2012-03-10 22:21:23 +0000

    * *.tcl:                Bumped the version number to 5.6; updated the 	* COPYRIGHT.txt:        copyright information. 	* README.txt: 	* ../../examples/*.tcl:

--- modules/tablelist/tablelist.tcl
+++ modules/tablelist/tablelist.tcl
@@ -1,12 +1,12 @@
 #==============================================================================
 # Main Tablelist package module.
 #
-# Copyright (c) 2000-2011  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2000-2012  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
 package require Tcl 8
 package require Tk  8
-package require -exact tablelist::common 5.5
+package require -exact tablelist::common 5.6
 
 package provide tablelist $::tablelist::version
 package provide Tablelist $::tablelist::version
