b3180642c41376590177b3a5d67b1b21e4e3f301
Csaba Nemethi <csaba.nemethi@t-online.de>
2012-03-10 22:21:23 +0000

    * *.tcl:                Bumped the version number to 5.6; updated the 	* COPYRIGHT.txt:        copyright information. 	* README.txt: 	* ../../examples/*.tcl:

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -1,28 +1,28 @@
 #==============================================================================
 # Tablelist and Tablelist_tile package index file.
 #
-# Copyright (c) 2000-2011  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2000-2012  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
 #
 # Regular packages:
 #
-package ifneeded tablelist         5.5 \
+package ifneeded tablelist         5.6 \
 	[list source [file join $dir tablelist.tcl]]
-package ifneeded tablelist_tile    5.5 \
+package ifneeded tablelist_tile    5.6 \
 	[list source [file join $dir tablelist_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Tablelist         5.5 \
-	[list package require -exact tablelist	    5.5]
-package ifneeded Tablelist_tile    5.5 \
-	[list package require -exact tablelist_tile 5.5]
+package ifneeded Tablelist         5.6 \
+	[list package require -exact tablelist	    5.6]
+package ifneeded Tablelist_tile    5.6 \
+	[list package require -exact tablelist_tile 5.6]
 
 #
 # Code common to all packages:
 #
-package ifneeded tablelist::common 5.5 \
+package ifneeded tablelist::common 5.6 \
         "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
 	 source [list [file join $dir tablelistPublic.tcl]]"
