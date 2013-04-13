ad9c229915eb338ca2076eb0bd809d9ecb208bb9
csaba <csaba>
2013-04-13 18:40:58 +0000

    * ../../examples/*.tcl: Bumped the version number to 5.8.

--- examples/tablelist/config_tile.tcl
+++ examples/tablelist/config_tile.tcl
@@ -2,10 +2,10 @@
 # Demonstrates how to use a tablelist widget for displaying and editing the
 # configuration options of an arbitrary widget.
 #
-# Copyright (c) 2000-2012  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2000-2013  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package require tablelist_tile 5.7
+package require tablelist_tile 5.8
 
 namespace eval demo {
     #
