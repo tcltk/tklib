0ec9efaf9233cb60f365c8edea688cbb80d0f57a
csaba <csaba>
2013-04-13 18:40:58 +0000

    * ../../examples/*.tcl: Bumped the version number to 5.8.

--- examples/tablelist/browseTree_tile.tcl
+++ examples/tablelist/browseTree_tile.tcl
@@ -2,10 +2,10 @@
 # Demonstrates how to use a tablelist widget for displaying information about
 # the children of an arbitrary widget.
 #
-# Copyright (c) 2010-2012  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2010-2013  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package require tablelist_tile 5.7
+package require tablelist_tile 5.8
 
 namespace eval demo {
     variable dir [file dirname [info script]]
--- examples/tablelist/iwidgets.tcl
+++ examples/tablelist/iwidgets.tcl
@@ -7,11 +7,11 @@ exec wish "$0" ${1+"$@"}
 # widgets from the Iwidgets package and of the Tk core checkbutton and
 # menubutton widgets.
 #
-# Copyright (c) 2004-2012  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2004-2013  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
 package require Tk 8.4				;# because of "-compound"
-package require tablelist 5.7
+package require tablelist 5.8
 package require Iwidgets
 
 wm title . "Serial Line Configuration"
