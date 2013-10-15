fbe423e2fbb240f993ac85b2a158874d679b3cc0
csaba <csaba>
2013-10-15 16:19:50 +0000

    * ../../examples/*.tcl: Bumped the version number to 5.10.

--- examples/tablelist/embeddedWindows.tcl
+++ examples/tablelist/embeddedWindows.tcl
@@ -8,7 +8,7 @@ exec wish "$0" ${1+"$@"}
 # Copyright (c) 2004-2013  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package require tablelist 5.9
+package require tablelist 5.10
 
 wm title . "Tk Library Scripts"
 
