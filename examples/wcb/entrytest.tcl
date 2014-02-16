7f4de3f8d18f717dd7f3de27e49ccc260a649ec4
csaba <csaba>
2014-02-16 19:20:38 +0000

    * ../../examples/*.tcl: Bumped the version number to 3.5.

--- examples/wcb/entrytest.tcl
+++ examples/wcb/entrytest.tcl
@@ -7,7 +7,7 @@ exec wish "$0" ${1+"$@"}
 #	wcb::postInsertEntryText <entry> <index> <string>
 #	wcb::cancel
 #
-# Copyright (c) 1999-2010  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 1999-2014  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
 package require wcb
