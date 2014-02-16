36ffc8ba5c2e4f22c2dd9e623e6db666490b0b3c
csaba <csaba>
2014-02-16 13:39:41 +0000

    * *.tcl:         Bumped the version number to 5.11. 	* COPYRIGHT.txt: 	* README.txt:

--- modules/tablelist/tablelistPublic.tcl
+++ modules/tablelist/tablelistPublic.tcl
@@ -1,14 +1,14 @@
 #==============================================================================
 # Main Tablelist and Tablelist_tile package module.
 #
-# Copyright (c) 2000-2013  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2000-2014  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
 namespace eval ::tablelist {
     #
     # Public variables:
     #
-    variable version	5.10
+    variable version	5.11
     variable library
     if {$tcl_version >= 8.4} {
 	set library	[file normalize [DIR]]
