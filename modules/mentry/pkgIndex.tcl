34534b4e5e6c1fe5643c777c8551f46bf97fe768
csaba <csaba>
2014-02-16 20:22:56 +0000

    * *.tcl:         Bumped the version number to 3.7; updated the 	* COPYRIGHT.txt: copyright information. 	* README.txt:

--- modules/mentry/pkgIndex.tcl
+++ modules/mentry/pkgIndex.tcl
@@ -1,28 +1,28 @@
 #==============================================================================
 # Mentry and Mentry_tile package index file.
 #
-# Copyright (c) 1999-2012  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 1999-2014  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
 #
 # Regular packages:
 #
-package ifneeded mentry         3.6 \
+package ifneeded mentry         3.7 \
 	[list source [file join $dir mentry.tcl]]
-package ifneeded mentry_tile    3.6 \
+package ifneeded mentry_tile    3.7 \
 	[list source [file join $dir mentry_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Mentry         3.6 \
-	[list package require -exact mentry      3.6]
-package ifneeded Mentry_tile    3.6 \
-	[list package require -exact mentry_tile 3.6]
+package ifneeded Mentry         3.7 \
+	[list package require -exact mentry      3.7]
+package ifneeded Mentry_tile    3.7 \
+	[list package require -exact mentry_tile 3.7]
 
 #
 # Code common to all packages:
 #
-package ifneeded mentry::common 3.6 \
+package ifneeded mentry::common 3.7 \
 	"namespace eval ::mentry { proc DIR {} {return [list $dir]} } ;\
 	 source [list [file join $dir mentryPublic.tcl]]"
