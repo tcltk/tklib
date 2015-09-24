dc3f5e459198b249ba832158cdf3973282948666
Sean Woods <yoda@etoyoc.com>
2015-09-24 19:44:17 +0000

    Updated the SHED definition to the latest standard.
    Added a "make shed" to update the manifest

--- tool/tool.tcl
+++ tool/tool.tcl
@@ -1,50 +1,19 @@
-set SCRIPT [file normalize [info script]]
-set TOOL_ROOT [file dirname [file dirname $SCRIPT]]
-set cwd [file dirname $SCRIPT]
-if {[catch {package require shed}]} {
-  puts "ADDING TOOL TO PKG PATH"
-  lappend auto_path [file join $cwd .. .. shed modules]
-  package require tool
-  package require shed
-}
-
-tool begin tklib
-###
-# Build basic description of this tool
-###
-tool description: {
-The reference implementation for the TOOL and SHED description languages.
-}
-tool distribution: fossil
-tool class: sak
-
-###
-# List of mirrors
-###
-tool mirror http://core.tcl.tk/tklib
-tool mirror http://fossil.etoyoc.com/fossil/tklib
-
 ###
 # Populate the branches
 ###
-tool branch trunk {
-  version: 0.7b
-  release: beta
-}
-tool branch tklib-0-6 {
+my release add tklib-0-6 {
   version: 0.6
-  release: final
+  checkout: tklib-0-6
 }
 
-foreach file [glob [file join $TOOL_ROOT apps *]] {
+foreach file [glob [file join $::TOOL_ROOT apps *]] {
   if {[file extension $file] ne {}} continue
-  application_scan $file
+  my application scan $file
 }
 
 ###
 # Build the module section
 ###
-foreach path [glob [file join $TOOL_ROOT modules *]] {
-  tool module_path [file tail $path] $path
+foreach path [glob [file join $::TOOL_ROOT modules *]] {
+  my module scan [file tail $path] $path
 }
-tool write [file join [file dirname $SCRIPT] tool.shed]
