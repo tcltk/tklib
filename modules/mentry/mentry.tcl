fcc33dd764e78db14d08106437e52a7edf637c56
csaba <csaba>
2018-10-08 11:56:58 +0000

    * *.tcl: Bumped the version number to 3.8; adapted to some changes in 	  future Tcl and Tk releases (TIP 278, independent Tcl and Tk versions); 	  updated the copyright information.

--- modules/mentry/mentry.tcl
+++ modules/mentry/mentry.tcl
@@ -1,10 +1,10 @@
 #==============================================================================
 # Main Mentry package module.
 #
-# Copyright (c) 1999-2014  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 1999-2018  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package require -exact mentry::common 3.7
+package require -exact mentry::common 3.8
 
 package provide Mentry $::mentry::version
 package provide mentry $::mentry::version
