f719f552d5cd5ffcd4f0222cfae5fc0e9c9ec6b6
csaba <csaba>
2019-04-14 17:52:45 +0000

    * *.tcl:         Bumped the version number to 6.5. 	* COPYRIGHT.txt: 	* README.txt:

--- modules/tablelist/pkgIndex.tcl
+++ modules/tablelist/pkgIndex.tcl
@@ -7,22 +7,22 @@
 #
 # Regular packages:
 #
-package ifneeded tablelist         6.4 \
+package ifneeded tablelist         6.5 \
 	[list source [file join $dir tablelist.tcl]]
-package ifneeded tablelist_tile    6.4 \
+package ifneeded tablelist_tile    6.5 \
 	[list source [file join $dir tablelist_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Tablelist         6.4 \
-	[list package require -exact tablelist	    6.4]
-package ifneeded Tablelist_tile    6.4 \
-	[list package require -exact tablelist_tile 6.4]
+package ifneeded Tablelist         6.5 \
+	[list package require -exact tablelist	    6.5]
+package ifneeded Tablelist_tile    6.5 \
+	[list package require -exact tablelist_tile 6.5]
 
 #
 # Code common to all packages:
 #
-package ifneeded tablelist::common 6.4 \
+package ifneeded tablelist::common 6.5 \
         "namespace eval ::tablelist { proc DIR {} {return [list $dir]} } ;\
 	 source [list [file join $dir tablelistPublic.tcl]]"
