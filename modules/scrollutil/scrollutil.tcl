be5118d04f416217f516e215df9a015ea006af94
csaba <csaba>
2020-01-07 20:10:02 +0000

    * *.tcl:         Bumped the version number to 1.4; updated the copyright 	* COPYRIGHT.txt: information.

--- modules/scrollutil/scrollutil.tcl
+++ modules/scrollutil/scrollutil.tcl
@@ -1,15 +1,19 @@
 #==============================================================================
 # Main Scrollutil package module.
 #
-# Copyright (c) 2019  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+# Copyright (c) 2019-2020  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package require -exact scrollutil::common 1.3
+package require -exact scrollutil::common 1.4
 
 package provide scrollutil $::scrollutil::version
 package provide Scrollutil $::scrollutil::version
 
 ::scrollutil::useTile 0
+
 ::scrollutil::sa::createBindings
 ::scrollutil::ss::createBindings
 ::scrollutil::sf::createBindings
+if {[package vcompare $::tk_version "8.4"] >= 0} {
+    ::scrollutil::createBindings
+}
