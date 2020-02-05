5996939a59a888f07e4595c0afa14bdbe99455b3
csaba <csaba>
2020-02-05 18:07:17 +0000

    * *.tcl:         Bumped the version number to 1.5. 	* COPYRIGHT.txt: 	* README.txt:

--- modules/scrollutil/pkgIndex.tcl
+++ modules/scrollutil/pkgIndex.tcl
@@ -7,21 +7,21 @@
 #
 # Regular packages:
 #
-package ifneeded scrollutil         1.4 \
+package ifneeded scrollutil         1.5 \
 	[list source [file join $dir scrollutil.tcl]]
-package ifneeded scrollutil_tile    1.4 \
+package ifneeded scrollutil_tile    1.5 \
 	[list source [file join $dir scrollutil_tile.tcl]]
 
 #
 # Aliases:
 #
-package ifneeded Scrollutil         1.4 \
-	[list package require -exact scrollutil      1.4]
-package ifneeded Scrollutil_tile    1.4 \
-	[list package require -exact scrollutil_tile 1.4]
+package ifneeded Scrollutil         1.5 \
+	[list package require -exact scrollutil      1.5]
+package ifneeded Scrollutil_tile    1.5 \
+	[list package require -exact scrollutil_tile 1.5]
 
 #
 # Code common to all packages:
 #
-package ifneeded scrollutil::common 1.4 \
+package ifneeded scrollutil::common 1.5 \
 	[list source [file join $dir scrollutilCommon.tcl]]
