20bc0ed9a97fc8977ab6bb0a863e0806a6502843
csaba <csaba>
2020-09-08 16:48:53 +0000

    * scripts/utils:               Moved mwutil.tcl and scaleutil.tcl to 	* scripts/utils/mwutil.tcl:    the new subdirectory utils and made them 	* scripts/utils/scaleutil.tcl: to packages; updated mwutil.tcl to 	* scripts/utils/pkgIndex.tcl:  version 2.17, like in Tablelist 6.11.

---
+++ modules/scrollutil/scripts/utils/pkgIndex.tcl
@@ -0,0 +1,8 @@
+#==============================================================================
+# mwutil and scaleutil package index file.
+#
+# Copyright (c) 2020  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
+#==============================================================================
+
+package ifneeded mwutil    2.17 [list source [file join $dir mwutil.tcl]]
+package ifneeded scaleutil 1.1  [list source [file join $dir scaleutil.tcl]]
