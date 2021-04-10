9f11ae4352f776c881cb535afdda2c06624c99f6
csaba <csaba>
2021-04-10 15:50:04 +0000

    * scripts/mwutil/*.tcl: Updated the mwutil package to version 2.19, 	  like in Tablelist6.13.

--- modules/mentry/scripts/mwutil/pkgIndex.tcl
+++ modules/mentry/scripts/mwutil/pkgIndex.tcl
@@ -4,4 +4,4 @@
 # Copyright (c) 2020-2021  Csaba Nemethi (E-mail: csaba.nemethi@t-online.de)
 #==============================================================================
 
-package ifneeded mwutil 2.18 [list source [file join $dir mwutil.tcl]]
+package ifneeded mwutil 2.19 [list source [file join $dir mwutil.tcl]]
