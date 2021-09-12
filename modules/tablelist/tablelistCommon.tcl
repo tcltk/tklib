69f5e1dcf96296f3ae6efd120a7eeda726a42940
csaba <csaba>
2021-09-12 12:12:22 +0000

    * *.tcl:              Bumped the version number to 6.15.1. 	* COPYRIGHT.txt: 	* README.txt: 	* doc/index.html: 	* doc/tablelist.html:

--- modules/tablelist/tablelistCommon.tcl
+++ modules/tablelist/tablelistCommon.tcl
@@ -8,7 +8,7 @@ namespace eval ::tablelist {
     #
     # Public variables:
     #
-    variable version	6.15
+    variable version	6.15.1
     variable library
     if {$::tcl_version >= 8.4} {
 	set library	[file dirname [file normalize [info script]]]
