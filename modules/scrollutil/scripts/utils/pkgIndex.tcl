4d2f283a4be5d223f35fc5ef2e062548e40b2813
csaba <csaba>
2021-12-21 10:23:52 +0000

    * scripts/utils/pkgIndex.tcl: Bumped the scaleutil version to 1.7.

--- modules/scrollutil/scripts/utils/pkgIndex.tcl
+++ modules/scrollutil/scripts/utils/pkgIndex.tcl
@@ -5,4 +5,4 @@
 #==============================================================================
 
 package ifneeded mwutil    2.19 [list source [file join $dir mwutil.tcl]]
-package ifneeded scaleutil 1.6  [list source [file join $dir scaleutil.tcl]]
+package ifneeded scaleutil 1.7  [list source [file join $dir scaleutil.tcl]]
