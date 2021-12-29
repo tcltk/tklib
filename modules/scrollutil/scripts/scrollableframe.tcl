b9753685fd3aa0401ac4f662fbdce08de2ffa590
csaba <csaba>
2021-12-29 13:43:53 +0000

    * scripts/scrollableframe.tcl: Added code that handles the virtual 	  event <<NoManagedChild>>, sent by Tk 8.7a3 and later to the content 	  frame of a scrollableframe when its last widget managed via pack or 	  grid becomes unmanaged (see TIP 518).

--- modules/scrollutil/scripts/scrollableframe.tcl
+++ modules/scrollutil/scripts/scrollableframe.tcl
@@ -156,6 +156,10 @@ proc scrollutil::sf::createBindings {} {
 	bind $class <ButtonRelease-1> \
 	    [list scrollutil::sf::onButtonRelease1 %W $isCf]
     }
+
+    bind ScrollableframeCf <<NoManagedChild>> {
+	scrollutil::sf::onNoManagedChild %W
+    }
 }
 
 #
@@ -1080,4 +1084,22 @@ proc scrollutil::sf::onButtonRelease1 {w isCf} {
     }
     variable origCursor
     ::$win configure -cursor $origCursor
+}
+
+#------------------------------------------------------------------------------
+# scrollutil::sf::onNoManagedChild
+#------------------------------------------------------------------------------
+proc scrollutil::sf::onNoManagedChild cf {
+    set win [winfo parent [winfo parent $cf]]
+    upvar ::scrollutil::ns${win}::data data
+
+    if {!$data(-fitcontentwidth) && $data(-contentwidth) <= 0} {
+	$cf configure -width 1
+	$cf configure -width $data(-contentwidth)
+    }
+
+    if {!$data(-fitcontentheight) && $data(-contentheight) <= 0} {
+	$cf configure -height 1
+	$cf configure -height $data(-contentheight)
+    }
 }
