e9174138c6118131271815841d84f76eefeb89c3
csaba <csaba>
2022-05-30 08:36:57 +0000

    * scripts/utils/pkgIndex.tcl:  Made the detection of the desktop 	* scripts/utils/scaleutil.tcl: environment when calculating the scaling 	  percentage corresponding to the display's DPI scaling level on X11 	  more reliable; removed the TEntry scaling for "vista" and "xpnative".

--- modules/tablelist/scripts/utils/pkgIndex.tcl
+++ modules/tablelist/scripts/utils/pkgIndex.tcl
@@ -5,6 +5,6 @@
 #==============================================================================
 
 package ifneeded mwutil    2.19 [list source [file join $dir mwutil.tcl]]
-package ifneeded scaleutil 1.8  [list source [file join $dir scaleutil.tcl]]
+package ifneeded scaleutil 1.9  [list source [file join $dir scaleutil.tcl]]
 package ifneeded scaleutilmisc 1.3 \
 	[list source [file join $dir scaleutilMisc.tcl]]
