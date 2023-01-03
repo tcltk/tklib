d23987102fa7b0c31c179157b27c770ddbead827
Andreas Kupries <andreas.kupries@gmail.com>
2023-01-03 21:36:00 +0000

    map demo layers: checkbuttons for selective display; added point display

--- examples/map/demo_map_layers.tcl
+++ examples/map/demo_map_layers.tcl
@@ -39,10 +39,12 @@ package require map::box::map-display
 package require map::box::store::fs
 package require map::box::store::memory
 #
+package require map::point::map-display
+package require map::point::store::fs
+package require map::point::store::memory
 
 ## TODO :: notebook, panels for area/track/box tables
 ## TODO :: panel widget for notebook -> shift sizes of map vs tables
-## TODO :: check boxes to enanle/disable the layers
 ## TODO :: look into stacking - outer areas/boxes can block inner such
 
 # # ## ### ##### ######## ############# #####################
@@ -68,6 +70,8 @@ proc usage {} {
 }
 
 proc do {cachedir datadir} {
+    global LA LT LB LP
+
     file mkdir            $cachedir
     map provider osm TILE $cachedir
 
@@ -80,15 +84,25 @@ proc do {cachedir datadir} {
 
     map track store fs     TFS  $datadir
     map track store memory TMEM TFS
+
     map area store fs      AFS  $datadir
     map area store memory  AMEM AFS
+
     map box store fs       BFS  $datadir
     map box store memory   BMEM BFS
+
+    map point store fs     PFS  $datadir
+    map point store memory PMEM PFS
 
     # UI elements
 
     button .m.exit   -command ::exit -text Exit
     button .m.rehome -command rehome -text Home
+
+    checkbutton .m.track -text Tracks -variable LT -command layer-tracks
+    checkbutton .m.area  -text Areas  -variable LA -command layer-areas
+    checkbutton .m.box   -text Boxes  -variable LB -command layer-boxes
+    checkbutton .m.point -text Points -variable LP -command layer-points
 
     map display .m.map \
 	-provider     TILE \
@@ -114,30 +128,48 @@ proc do {cachedir datadir} {
 	-rect-config { -stipple gray12 -fill black } \
 	;#-on-active   box-active-changed
 
-    # Show all ... TODO :: check boxes to selectively enable/disable layers
-    AREAS  enable
-    TRACKS enable
-    BOXES enable
+    map point map-display POINTS .m.map PMEM \
+	;# -on-active   point-active-changed
+
+    # Show all ...
+    AREAS  enable ; set LA 1
+    TRACKS enable ; set LT 1
+    BOXES  enable ; set LB 1
+    POINTS enable ; set LP 1
 
     # Layout
 
-    grid rowconfigure    .m 0 -weight 1
-    grid rowconfigure    .m 1 -weight 0
+    grid rowconfigure    .m 0 -weight 0
+    grid rowconfigure    .m 1 -weight 1
+    grid rowconfigure    .m 2 -weight 0
 
     grid columnconfigure .m 0 -weight 0
-    grid columnconfigure .m 1 -weight 1
+    grid columnconfigure .m 1 -weight 0
     grid columnconfigure .m 2 -weight 0
-    grid columnconfigure .m 3 -weight 0
+    grid columnconfigure .m 3 -weight 1
+    grid columnconfigure .m 4 -weight 0
+    grid columnconfigure .m 5 -weight 0
 
-    grid .m.map    -row 0 -column 0 -columnspan 4 -sticky swen
-    grid .m.exit   -row 1 -column 0               -sticky swen
-    grid .m.rehome -row 1 -column 1               -sticky swn
+    grid .m.track  -row 0 -column 0               -sticky swen
+    grid .m.area   -row 0 -column 1               -sticky swen
+    grid .m.box    -row 0 -column 2               -sticky swen
+    grid .m.point  -row 0 -column 3               -sticky swn
+
+    grid .m.map    -row 1 -column 0 -columnspan 6 -sticky swen
+
+    grid .m.exit   -row 2 -column 0               -sticky swen
+    grid .m.rehome -row 2 -column 1               -sticky swen
 
     return
 }
 
 proc rehome {} { .m.map center [home] }
 proc home   {} { return {51.667205 6.451442} } ;# Xanten Ampitheater/Coliseum
+
+proc layer-tracks {} { global LT ; if {$LT} { TRACKS enable } else { TRACKS disable } }
+proc layer-areas  {} { global LA ; if {$LA} { AREAS  enable } else { AREAS  disable } }
+proc layer-boxes  {} { global LB ; if {$LB} { BOXES  enable } else { BOXES  disable } }
+proc layer-points {} { global LP ; if {$LP} { POINTS enable } else { POINTS disable } }
 
 main
 exit 0
